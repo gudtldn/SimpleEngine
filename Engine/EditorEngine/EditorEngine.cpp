290ddc5cf9b4bda49196829101af799ea903607e
gudtldn <gudtldn1234@gmail.com>
2025-05-18 13:48:38 +0900

    ✨ feat: Windows 서브시스템으로 변경 및 wWinMain 함수로 진입점 수정

--- Engine/EditorEngine/EditorEngine.cpp
+++ Engine/EditorEngine/EditorEngine.cpp
@@ -1,6 +1,19 @@
+import std;
+#include <Windows.h>
 
-int main(int argc, char* argv[])
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd
+)
 {
-    
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
+    std::println("Hello World!");
     return 0;
 }
