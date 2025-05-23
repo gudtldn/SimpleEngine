4873547722823feece8cb44edda9c7f0698feb8c
gudtldn <gudtldn1234@gmail.com>
2025-05-24 03:43:16 +0900

    refactor: Windows 런처 진입점을 EditorEngine.cpp에서 LaunchWindows.cpp로 변경

---
+++ Engine/EditorEngine/Source/Launch/LaunchWindows.cpp
@@ -0,0 +1,55 @@
+﻿import std;
+import SimpleEngine.App;
+
+import <Windows.h>;
+import <SDL3/SDL.h>;
+
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd
+)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
+    Application::Startup(lpCmdLine);
+    Application::Shutdown();
+
+    // SDL_SetAppMetadata("My", "0.1.0", "com.example.simpleengine");
+    //
+    // SDL_Init(SDL_INIT_VIDEO);
+    // SDL_Window* Wnd;
+    // SDL_Renderer* Renderer;
+    // SDL_CreateWindowAndRenderer("My Window", 1280, 720, SDL_WINDOW_RESIZABLE, &Wnd, &Renderer);
+    //
+    // bool bRunning = true;
+    // while (bRunning)
+    // {
+    //     SDL_Event Event;
+    //     while (SDL_PollEvent(&Event))
+    //     {
+    //         switch (Event.type)
+    //         {
+    //         case SDL_EVENT_QUIT:
+    //             bRunning = false;
+    //             break;
+    //         }
+    //     }
+    //
+    //     SDL_RenderClear(Renderer);
+    //     SDL_RenderPresent(Renderer);
+    // }
+    //
+    // SDL_DestroyRenderer(Renderer);
+    // SDL_DestroyWindow(Wnd);
+    // SDL_Quit();
+
+    return 0;
+}
+
+
