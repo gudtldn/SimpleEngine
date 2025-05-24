4a03d1b1aeac4f1e3eb204d0132c82450124f8b5
gudtldn <gudtldn1234@gmail.com>
2025-05-24 15:32:38 +0900

    feat: 플랫폼별 매크로 정의 및 플랫폼 헤더 파일 추가

---
+++ Engine/EngineCore/Source/Platform/PlatformMacros.h
@@ -0,0 +1,37 @@
+﻿// ReSharper disable CppClangTidyClangDiagnosticUnusedMacros
+#pragma once
+
+// Windows
+#if defined(_WIN32) || defined(_WIN64)
+    #define PLATFORM_WINDOWS 1
+// Linux
+#elif defined(__linux__)
+    #define PLATFORM_LINUX 1
+// MacOS
+#elif defined(__APPLE__)
+    #define PLATFORM_MACOS 1
+#else
+    #define PLATFORM_UNKNOWN 1
+#endif
+
+
+#if !defined(PLATFORM_WINDOWS)
+    #define PLATFORM_WINDOWS 0
+#endif
+
+#if !defined(PLATFORM_LINUX)
+    #define PLATFORM_LINUX 0
+#endif
+
+#if !defined(PLATFORM_MACOS)
+    #define PLATFORM_MACOS 0
+#endif
+
+
+#if PLATFORM_WINDOWS
+    #include "Platform/Windows/WindowsPlatform.h"
+#elif PLATFORM_LINUX
+    #include "Platform/Linux/LinuxPlatform.h"
+#elif PLATFORM_MACOS
+    #include "Platform/MacOS/MacOSPlatform.h"
+#endif
