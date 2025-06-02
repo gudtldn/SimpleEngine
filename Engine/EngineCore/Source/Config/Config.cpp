bfa42a9bf0a475259b5a89953b1c02309614e493
gudtldn <gudtldn1234@gmail.com>
2025-06-03 03:04:40 +0900

    wip: 기본 toml파일 읽는 함수 추가

--- Engine/EngineCore/Source/Config/Config.cpp
+++ Engine/EngineCore/Source/Config/Config.cpp
@@ -1,3 +1,25 @@
-﻿module SimpleEngine.Config;
+﻿module;
+#define TOML_EXCEPTIONS 0
+#include <toml++/toml.h>
+module SimpleEngine.Config;
+
+import SimpleEngine.Logging;
 
 
+namespace se::config
+{
+TomlResult Config::ReadConfig(const std::filesystem::path& config_file_path)
+{
+    toml::parse_result result = toml::parse_file(config_file_path.generic_string());
+    if (result.failed())
+    {
+        const toml::parse_error& err = result.error();
+        ConsoleLog(ELogLevel::Error, u8"Failed to parse config file: {}", err.description());
+
+        return std::unexpected{err};
+    }
+
+    config_table = std::move(result.table());
+    return {};
+}
+}
--- Engine/EngineCore/Source/Config/Config.cppm
+++ Engine/EngineCore/Source/Config/Config.cppm
@@ -1,3 +1,21 @@
-﻿export module SimpleEngine.Config;
+﻿module;
+#define TOML_EXCEPTIONS 0
+#include <toml++/toml.h>
+export module SimpleEngine.Config;
+
+import SimpleEngine.Platform.Types;
+import std;
 
 
+namespace se::config
+{
+using TomlResult = std::expected<void, toml::parse_error>;
+
+export class Config
+{
+    TomlResult ReadConfig(const std::filesystem::path& config_file_path);
+
+private:
+    toml::table config_table;
+};
+}
