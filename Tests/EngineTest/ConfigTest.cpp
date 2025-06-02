8a59768b97e1785e19f8ff0f41b209ebfdff10fb
gudtldn <gudtldn1234@gmail.com>
2025-06-03 04:17:40 +0900

    feat: Config 클래스 테스트 케이스 추가 및 EngineTest 설정 수정

---
+++ Tests/EngineTest/ConfigTest.cpp
@@ -0,0 +1,15 @@
+﻿#include "doctest.h"
+
+import SimpleEngine.Config;
+
+
+TEST_SUITE("SimpleEngine.Config")
+{
+TEST_CASE("not exist config file")
+{
+    se::config::Config config;
+    const se::config::ParseResult v = config.ReadConfig("Test.t1oml");
+
+    CHECK(!v.has_value());
+}
+}
--- Tests/EngineTest/MainTest.cpp
+++ Tests/EngineTest/MainTest.cpp
@@ -1,10 +1,5 @@
+// ReSharper disable CppUnusedIncludeDirective
 #include <time.h>  // NOLINT(modernize-deprecated-headers)
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-
-
-TEST_CASE("Test")
-{
-    CHECK(true);
-}
