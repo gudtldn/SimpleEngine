fc0ee4cef9e4f31fc2b81c2e0aa946c9278227fe
gudtldn <gudtldn1234@gmail.com>
2025-06-04 09:15:01 +0900

    fix: Config 클래스의 ReadConfig 정적 함수 호출로 테스트 코드 수정

--- Tests/EngineTest/ConfigTest.cpp
+++ Tests/EngineTest/ConfigTest.cpp
@@ -7,8 +7,7 @@ TEST_SUITE("SimpleEngine.Config")
 {
 TEST_CASE("not exist config file")
 {
-    se::config::Config config;
-    const se::config::ParseResult v = config.ReadConfig("Test.t1oml");
+    const se::config::ParseResult v = se::config::Config::ReadConfig(u8"Test.t1oml");
 
     CHECK(!v.has_value());
 }
