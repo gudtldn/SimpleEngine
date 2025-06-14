124108758660b7bb073d252eacad5ecdefca8e80
gudtldn <gudtldn1234@gmail.com>
2025-06-15 00:16:46 +0900

    feat: 플랫폼 서브시스템에서 SDL 초기화 및 윈도우 생성 코드 추가

--- Engine/EngineCore/Source/Subsystems/PlatformSubsystem/PlatformSubsystem.cpp
+++ Engine/EngineCore/Source/Subsystems/PlatformSubsystem/PlatformSubsystem.cpp
@@ -1,12 +1,71 @@
 ﻿module SimpleEngine.Subsystems.PlatformSubsystem;
 
+import SimpleEngine.Logging;
+import <SDL3/SDL_gpu.h>;
+
+
+PlatformSubsystem::PlatformSubsystem(uint32 in_sdl_init_flags)
+    : sdl_init_flags(in_sdl_init_flags)
+{
+}
+
+void PlatformSubsystem::PrepareWindow(const std::u8string& window_title, uint32 window_width, uint32 window_height, uint32 sdl_window_flags)
+{
+    window_info.title = window_title;
+    window_info.width = window_width;
+    window_info.height = window_height;
+    window_info.sdl_window_flags = sdl_window_flags;
+}
 
 bool PlatformSubsystem::Initialize()
 {
-    // TODO: Implements this
+    ConsoleLog(ELogLevel::Info, u8"Initializing Platform Subsystem...");
+    if (!SDL_Init(sdl_init_flags))
+    {
+        ConsoleLog(ELogLevel::Error, u8"SDL_Init failed: {}", SDL_GetError());
+        return false;
+    }
+    ConsoleLog(ELogLevel::Info, u8"SDL_Init succeeded");
+
+    // TODO: 나중에 다중모니터 지원하도록 변경
+    ConsoleLog(ELogLevel::Info, u8"Initializing Window...");
+    const float main_display_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    const char* window_title_c = reinterpret_cast<const char*>(window_info.title.c_str());
+    window = SDL_CreateWindow(
+        window_title_c,
+        static_cast<int>(static_cast<float>(window_info.width) * main_display_scale),
+        static_cast<int>(static_cast<float>(window_info.height) * main_display_scale),
+        window_info.sdl_window_flags
+    );
+
+    if (!window)
+    {
+        ConsoleLog(ELogLevel::Error, u8"SDL_CreateWindow failed: {}", SDL_GetError());
+        return false;
+    }
+    SDL_ShowWindow(window);
+
     return true;
 }
 
 void PlatformSubsystem::Release()
 {
+    ConsoleLog(ELogLevel::Info, u8"Releasing Platform Subsystem...");
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+    }
+    SDL_Quit();
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void PlatformSubsystem::PollEvents(std::vector<SDL_Event>& out_events)
+{
+    // TODO: Event Dispatcher 만들기
+    out_events.clear();
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        out_events.push_back(event);
+    }
 }
--- Engine/EngineCore/Source/Subsystems/PlatformSubsystem/PlatformSubsystem.cppm
+++ Engine/EngineCore/Source/Subsystems/PlatformSubsystem/PlatformSubsystem.cppm
@@ -1,5 +1,6 @@
 ﻿export module SimpleEngine.Subsystems.PlatformSubsystem;
 
+import SimpleEngine.Platform.Types;
 import SimpleEngine.Core.ISubsystem;
 import std;
 import <SDL3/SDL.h>;
@@ -7,13 +8,52 @@ import <SDL3/SDL.h>;
 
 export class PlatformSubsystem : public ISubsystem
 {
+    struct WindowInfo
+    {
+        std::u8string title;
+        uint32 width;
+        uint32 height;
+        uint32 sdl_window_flags;
+    };
+
 public:
+    /**
+     * PlatformSubsystem을 새로 생성합니다.
+     * @param in_sdl_init_flags SDL_Init에 들어갈 Flag목록
+     */
+    explicit PlatformSubsystem(
+        uint32 in_sdl_init_flags =
+            SDL_INIT_VIDEO
+            | SDL_INIT_AUDIO
+            | SDL_INIT_GAMEPAD
+            | SDL_INIT_EVENTS
+    );
+
+    /**
+     * SDL 윈도우 초기화 및 생성 준비를 수행합니다.
+     * @param window_title 창 이름
+     * @param window_width 창 너비
+     * @param window_height 창 높이
+     * @param sdl_window_flags SDL_WindowFlags 조합 비트마스크
+     */
+    void PrepareWindow(
+        const std::u8string& window_title,
+        uint32 window_width,
+        uint32 window_height,
+        uint32 sdl_window_flags
+    );
+
     [[nodiscard]] virtual bool Initialize() override;
     virtual void Release() override;
+
+    void PollEvents(std::vector<SDL_Event>& out_events);
 
 public:
     SDL_Window* GetWindow() const { return window; }
 
 private:
+    const uint32 sdl_init_flags;
+
+    WindowInfo window_info;
     SDL_Window* window = nullptr;
 };
