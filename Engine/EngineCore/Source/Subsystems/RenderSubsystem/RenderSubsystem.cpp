cb4a0c78217a2d9cd31c8eb833ab949369dcce1d
gudtldn <gudtldn1234@gmail.com>
2025-06-16 00:28:30 +0900

    feat: RenderSubsystem 초기화 로직 개선 및 스왑체인 설정 기능 추가

--- Engine/EngineCore/Source/Subsystems/RenderSubsystem/RenderSubsystem.cpp
+++ Engine/EngineCore/Source/Subsystems/RenderSubsystem/RenderSubsystem.cpp
@@ -1,19 +1,75 @@
 ﻿module SimpleEngine.Subsystems.RenderSubsystem;
 
+import SimpleEngine.Logging;
 import SimpleEngine.Subsystems;
 import SimpleEngine.Subsystems.PlatformSubsystem;
+import <SDL3/SDL_gpu.h>;
 
 
 bool RenderSubsystem::Initialize()
 {
+    ConsoleLog(ELogLevel::Info, u8"Initializing Render subsystem...");
+
     const PlatformSubsystem* platform_subsystem = GetSubsystem<PlatformSubsystem>();
     cached_window = platform_subsystem->GetWindow();
 
+    // Window가 존재하는지 확인
+    if (!cached_window)
+    {
+        ConsoleLog(ELogLevel::Error, u8"Window not found. Render subsystem cannot be initialized.");
+        return false;
+    }
+
+    // 지원할 셰이더 포맷들 설정
+    const SDL_PropertiesID props = SDL_CreateProperties();
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_SPIRV_BOOLEAN, true);
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_DXIL_BOOLEAN, true);
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_MSL_BOOLEAN, true);
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_SHADERS_METALLIB_BOOLEAN, true);
+
+#ifdef _DEBUG
+    // 디버그 모드 설정
+    SDL_SetBooleanProperty(props, SDL_PROP_GPU_DEVICE_CREATE_DEBUGMODE_BOOLEAN, true);
+#endif
+
+    gpu_device = SDL_CreateGPUDeviceWithProperties(props);
+    SDL_DestroyProperties(props);
+
+    if (!gpu_device)
+    {
+        ConsoleLog(ELogLevel::Error, u8"SDL_CreateGPUDeviceWithProperties failed: {}", SDL_GetError());
+        return false;
+    }
+
+    // Window를 GPU Device에 연결
+    if (!SDL_ClaimWindowForGPUDevice(gpu_device, cached_window))
+    {
+        ConsoleLog(ELogLevel::Error, u8"SDL_ClaimWindowForGPUDevice failed: {}", SDL_GetError());
+        SDL_DestroyGPUDevice(gpu_device);
+        gpu_device = nullptr;
+        return false;
+    }
+
+    if (!SDL_SetGPUSwapchainParameters(gpu_device, cached_window, swapchain_composition, present_mode))
+    {
+        ConsoleLog(ELogLevel::Warning, u8"SDL_SetGPUSwapchainParameters failed: {}", SDL_GetError());
+    }
+
+    ConsoleLog(ELogLevel::Info, u8"Window and GPU device created successfully");
     return true;
 }
 
 void RenderSubsystem::Release()
 {
+    if (gpu_device)
+    {
+        if (cached_window)
+        {
+            SDL_ReleaseWindowFromGPUDevice(gpu_device, cached_window);
+        }
+        SDL_DestroyGPUDevice(gpu_device);
+        gpu_device = nullptr;
+    }
 }
 
 std::vector<std::type_index> RenderSubsystem::GetDependencies() const
@@ -21,4 +77,10 @@ std::vector<std::type_index> RenderSubsystem::GetDependencies() const
     return {
         typeid(PlatformSubsystem)
     };
+}
+
+void RenderSubsystem::ConfigureSwapchain(SDL_GPUSwapchainComposition in_composition, SDL_GPUPresentMode in_present_mode)
+{
+    swapchain_composition = in_composition;
+    present_mode = in_present_mode;
 }
--- Engine/EngineCore/Source/Subsystems/RenderSubsystem/RenderSubsystem.cppm
+++ Engine/EngineCore/Source/Subsystems/RenderSubsystem/RenderSubsystem.cppm
@@ -13,10 +13,19 @@ public:
 
     virtual std::vector<::std::type_index> GetDependencies() const override;
 
+    /**
+     * 스왑체인 파라미터를 미리 설정합니다.
+     * @note 이 함수는 Initialize()보다 먼저 호출되어야 합니다.
+     */
+    void ConfigureSwapchain(SDL_GPUSwapchainComposition in_composition, SDL_GPUPresentMode in_present_mode);
+
 public:
-    SDL_GPUDevice* GetGpuDevice() const { return gpu_device; }
+    [[nodiscard]] SDL_GPUDevice* GetGpuDevice() const { return gpu_device; }
 
 private:
     SDL_GPUDevice* gpu_device = nullptr;
     SDL_Window* cached_window = nullptr;
+
+    SDL_GPUSwapchainComposition swapchain_composition = SDL_GPU_SWAPCHAINCOMPOSITION_SDR;
+    SDL_GPUPresentMode present_mode = SDL_GPU_PRESENTMODE_VSYNC;
 };
