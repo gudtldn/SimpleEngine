8c9cbbed5fb0e48bb074f3d77d0eaf2b8758bdaf
gudtldn <gudtldn1234@gmail.com>
2025-07-01 19:14:07 +0900

    feat: StringUtils 개선 및 테스트 추가

--- Engine/EngineCore/Source/Utils/StringUtils.cpp
+++ Engine/EngineCore/Source/Utils/StringUtils.cpp
@@ -1,8 +1,10 @@
 ﻿module;
 #include <utf8.h>
+#include <unicode/unistr.h>
 module SimpleEngine.Utils;
 import :StringUtils;
 
+import SimpleEngine.Platform.Types;
 import SimpleEngine.Core.TypeTraits;
 import <cassert>;
 
@@ -11,51 +13,51 @@ namespace se::string_utils
 {
 std::u8string ToU8String(std::string_view in_str)
 {
-    std::u8string result;
-    result.reserve(in_str.size());
-
-    // 일단 그냥 assign해보고, 추후 문제가 있으면 utf8::replace_invalid 추가로 사용
-    assert(utf8::is_valid(in_str.begin(), in_str.end()));
-
-    result.assign(in_str.begin(), in_str.end());
-    return result;
+    return std::u8string{
+        reinterpret_cast<const char8_t*>(in_str.data()),
+        in_str.size()
+    };
 }
 
 std::u8string ToU8String(std::wstring_view in_str)
 {
-    std::u8string result;
-    result.reserve(in_str.size());
+    const icu::UnicodeString ustr{ in_str.data(), static_cast<int32>(in_str.size()) };
 
-    if constexpr (sizeof(wchar_t) == 2)
-    {
-        utf8::utf16to8(in_str.begin(), in_str.end(), std::back_inserter(result));
-    }
-    else if constexpr (sizeof(wchar_t) == 4)
-    {
-        utf8::utf32to8(in_str.begin(), in_str.end(), std::back_inserter(result));
-    }
-    else
-    {
-        static_assert(sizeof(wchar_t) == 2 || sizeof(wchar_t) == 4, "Invalid wchar_t size");
-    }
-    return result;
+    std::string result;
+    ustr.toUTF8String(result);
+
+    return ToU8String(result);
 }
 
 std::u8string ToU8String(std::u16string_view in_str)
 {
-    std::u8string result;
-    result.reserve(in_str.size());
+    const icu::UnicodeString ustr{ in_str.data(), static_cast<int32>(in_str.size()) };
 
-    utf8::utf16to8(in_str.begin(), in_str.end(), std::back_inserter(result));
-    return result;
+    std::string result;
+    ustr.toUTF8String(result);
+
+    return ToU8String(result);
 }
 
 std::u8string ToU8String(std::u32string_view in_str)
 {
-    std::u8string result;
-    result.reserve(in_str.size());
+    static_assert(
+        sizeof(std::u32string_view::value_type) == sizeof(UChar32),
+        "char32_t and UChar32 have different sizes."
+    );
+    static_assert(
+        alignof(std::u32string_view::value_type) == alignof(UChar32),
+        "char32_t and UChar32 have different alignments."
+    );
 
-    utf8::utf32to8(in_str.begin(), in_str.end(), std::back_inserter(result));
-    return result;
+    const icu::UnicodeString ustr = icu::UnicodeString::fromUTF32(
+        reinterpret_cast<const UChar32*>(in_str.data()),
+        static_cast<int32_t>(in_str.length())
+    );
+
+    std::string result;
+    ustr.toUTF8String(result);
+
+    return ToU8String(result);
 }
 }
---
+++ Tests/EngineTest/StringUtilsTest.cpp
@@ -0,0 +1,40 @@
+﻿#include "doctest.h"
+
+import std;
+import SimpleEngine.Utils;
+
+
+TEST_SUITE("SimpleEngine.StringUtils")
+{
+using namespace se::string_utils;
+
+static const std::u8string ValidateString = u8"Hello World 안녕 こんちは 👍";
+
+TEST_CASE("ToU8String(std::string_view)")
+{
+    const std::string str = "Hello World 안녕 こんちは 👍";
+    const std::u8string u8str = ToU8String(str);
+    CHECK(u8str == ValidateString);
+}
+
+TEST_CASE("ToU8String(std::wstring_view)")
+{
+    const std::wstring wstr = L"Hello World 안녕 こんちは 👍";
+    const std::u8string u8str = ToU8String(wstr);
+    CHECK(u8str == ValidateString);
+}
+
+TEST_CASE("ToU8String(std::u16string_view)")
+{
+    const std::u16string u16str = u"Hello World 안녕 こんちは 👍";
+    const std::u8string u8str = ToU8String(u16str);
+    CHECK(u8str == ValidateString);
+}
+
+TEST_CASE("ToU8String(std::u32string_view)")
+{
+    const std::u32string u32str = U"Hello World 안녕 こんちは 👍";
+    const std::u8string u8str = ToU8String(u32str);
+    CHECK(u8str == ValidateString);
+}
+}
