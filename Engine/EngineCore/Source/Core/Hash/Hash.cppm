93f83f86802ed221d78f25fb7925fa46eab3c176
gudtldn <gudtldn1234@gmail.com>
2025-07-01 21:58:04 +0900

    refactor: FNV-1a 해시 함수 분리 및 StringName에서 재사용하도록 수정

---
+++ Engine/EngineCore/Source/Core/Hash/Hash.cppm
@@ -0,0 +1,29 @@
+﻿export module SimpleEngine.Core.Hash;
+
+import SimpleEngine.Platform.Types;
+import std;
+
+export namespace se::hash
+{
+/**
+ * FNV-1a 해시 알고리즘을 사용한 문자열 해싱 함수
+ * @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+ */
+template <typename StringType>
+constexpr uint64 FowlerNollVoHash(const StringType& in_str) noexcept
+{
+    using CharType = std::remove_cv_t<std::remove_pointer_t<decltype(std::data(in_str))>>;
+    static_assert(sizeof(CharType) == 1, u8"Only 1-byte character types are supported");
+
+    std::basic_string_view<CharType> sv = in_str;
+
+    uint64 hash = 0xcbf29ce484222325ULL; // FNV_offset_basis
+    for (const CharType c : sv)
+    {
+        const uint8 byte = static_cast<uint8>(c);
+        hash ^= byte;
+        hash *= 0x100000001b3ULL; // FNV_prime
+    }
+    return hash;
+}
+}
--- Engine/EngineCore/Source/Core/StringName/StringName.cpp
+++ Engine/EngineCore/Source/Core/StringName/StringName.cpp
@@ -1,25 +1,8 @@
 ﻿module SimpleEngine.Core.StringName;
 
+import SimpleEngine.Core.Hash;
 import SimpleEngine.Utils;
 
-
-namespace
-{
-/**
- * FNV-1a 해시 알고리즘을 사용한 문자열 해싱 함수
- * @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
- */
-constexpr uint64 ComputeHash(std::u8string_view in_str) noexcept
-{
-    uint64 hash = 0xcbf29ce484222325ULL;
-    for (const char8_t c : in_str)
-    {
-        hash ^= c;
-        hash *= 0x100000001b3ULL;
-    }
-    return hash;
-}
-}
 
 namespace se::core
 {
@@ -31,8 +14,8 @@ StringName::StringName(const char8* in_str)
 StringName::StringName(std::u8string_view in_str)
 {
     // TODO: StringNamePool에 등록해야함
-    display_hash = ComputeHash(in_str);
-    comparison_hash = ComputeHash(string_utils::ToU8LowerCase(in_str));
+    display_hash = hash::FowlerNollVoHash(in_str);
+    comparison_hash = hash::FowlerNollVoHash(string_utils::ToU8LowerCase(in_str));
 }
 
 std::u8string StringName::ToString() const
