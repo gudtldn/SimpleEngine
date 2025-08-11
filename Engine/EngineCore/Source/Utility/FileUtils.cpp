acd8391451b6b8ed081d05613483a7cc9f99c989
gudtldn <gudtldn1234@gmail.com>
2025-08-11 23:57:32 +0900

    feat: 파일 읽기 로직 개선 및 문자열 반환 함수 추가

--- Engine/EngineCore/Source/Utility/FileUtils.cpp
+++ Engine/EngineCore/Source/Utility/FileUtils.cpp
@@ -1,37 +1,50 @@
 ﻿module SimpleEngine.Utility;
+import :StringUtils;
 import :FileUtils;
 
 
 namespace se::utility::file_utils
 {
-FileResult<std::vector<uint8>> ReadFromBinary(const std::filesystem::path& file_path)
+FileResult<std::vector<uint8>> ReadToByteArray(const std::filesystem::path& file_path)
 {
-    if (!std::filesystem::exists(file_path))
-    {
-        return std::unexpected{ FileReadError::NotFound(u8"File not found: " + file_path.generic_u8string()) };
-    }
-
-    std::ifstream file(file_path, std::ios::ate | std::ios::binary);
+    std::ifstream file(file_path, std::ios::binary);
     if (!file.is_open())
     {
+        if (!std::filesystem::exists(file_path))
+        {
+            return std::unexpected{ FileReadError::NotFound(u8"File not found: " + file_path.generic_u8string()) };
+        }
+        if (!std::filesystem::is_regular_file(file_path))
+        {
+            return std::unexpected{ FileReadError::Format(u8"File is not a regular file: " + file_path.generic_u8string()) };
+        }
         return std::unexpected{ FileReadError::OpenFailed(u8"Failed to open file: " + file_path.generic_u8string()) };
     }
 
-    const size_t size = file.tellg();
-    if (size == 0)
+    std::error_code ec;
+    const uint64 file_size = std::filesystem::file_size(file_path, ec);
+    if (ec)
     {
-        return std::unexpected(FileReadError::EOF(u8"File is empty: " + file_path.generic_u8string()));
+        return std::unexpected{ FileReadError::EOF(u8"File size error: " + file_path.generic_u8string()) };
     }
 
-    std::vector<uint8> data(size);
-    file.seekg(0);
-
-    if (!file.read(reinterpret_cast<char*>(data.data()), size))
+    std::vector<uint8> data(file_size);
+    if (!file.read(reinterpret_cast<char*>(data.data()), file_size))
     {
         return std::unexpected(FileReadError::Read(u8"Failed to read file: " + file_path.generic_u8string()));
     }
 
     file.close();
     return data;
+}
+
+FileResult<std::u8string> ReadToString(const std::filesystem::path& file_path)
+{
+    const auto result = ReadToByteArray(file_path);
+    if (result.has_value())
+    {
+        return string_utils::ToU8String({ reinterpret_cast<const char*>(result->data()), result->size() });
+    }
+    return std::unexpected{ std::move(result).error() };
 }
 }
--- Engine/EngineCore/Source/Utility/FileUtils.cppm
+++ Engine/EngineCore/Source/Utility/FileUtils.cppm
@@ -38,6 +38,9 @@ struct FileReadError
 template <typename T>
 using FileResult = std::expected<T, FileReadError>;
 
-/** binary파일을 읽어 byte array로 반환합니다. */
-[[nodiscard]] FileResult<std::vector<uint8>> ReadFromBinary(const std::filesystem::path& file_path);
+/** 파일을 읽고, byte array로 반환합니다. */
+[[nodiscard]] FileResult<std::vector<uint8>> ReadToByteArray(const std::filesystem::path& file_path);
+
+/** 파일을 읽고, string으로 반환합니다. */
+[[nodiscard]] FileResult<std::u8string> ReadToString(const std::filesystem::path& file_path);
 }
