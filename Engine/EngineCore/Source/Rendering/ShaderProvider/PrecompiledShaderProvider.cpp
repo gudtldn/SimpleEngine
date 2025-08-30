43df3387aeb11305dc8e6f46c505627d9fa9d06e
gudtldn <gudtldn1234@gmail.com>
2025-08-30 20:10:53 +0900

    feat: 디버그 모드에서 SPIR-V 확장자 검증 추가

--- Engine/EngineCore/Source/Rendering/ShaderProvider/PrecompiledShaderProvider.cpp
+++ Engine/EngineCore/Source/Rendering/ShaderProvider/PrecompiledShaderProvider.cpp
@@ -8,6 +8,20 @@ namespace se::rendering::shader_provider
 {
 SDL_GPUShader* PrecompiledShaderProvider::Provide(const ShaderRequest& request)
 {
+    if constexpr (utility::IS_DEBUG_BUILD)
+    {
+        const std::u8string ext = request.source_path.extension().u8string();
+        if (!(
+            ext.contains(u8".spv")
+            || ext.contains(u8".spirv")
+            || ext.contains(u8".spvt")
+        ))
+        {
+            ConsoleLog(ELogLevel::Error, u8"Precompiled shader provider only supports SPIR-V files.");
+            return nullptr;
+        }
+    }
+
     return utility::shader_utils::CompileFromSPIRV(device, request.source_path);
 }
 }
