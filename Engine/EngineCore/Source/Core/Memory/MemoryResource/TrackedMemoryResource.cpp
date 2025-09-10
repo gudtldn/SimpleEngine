ef0bb2c964049cb7eb9d0283eaa62c9c0aeef2cd
gudtldn <gudtldn1234@gmail.com>
2025-09-11 01:45:43 +0900

    refactor: 메모리 할당 및 해제 로직 단순화 및 코드 정리

--- Engine/EngineCore/Source/Core/Memory/MemoryResource/TrackedMemoryResource.cpp
+++ Engine/EngineCore/Source/Core/Memory/MemoryResource/TrackedMemoryResource.cpp
@@ -18,40 +18,15 @@ void* TrackedMemoryResource::do_allocate(size_t size, size_t align)
         return nullptr;
     }
 
-    // 이런 느낌으로 할당
-    // |                raw_block                | <- 실제 할당된 메모리
-    // | padding |   header   |    user_data     | <- std::align으로 정렬 후 사용할 메모리
-
-    // 헤더, 데이터, 정렬 패딩을 모두 담을 공간을 계산
-    const size_t total_size_to_alloc = HEADER_SIZE + size + align - 1;
-
     // upstream에 할당을 요청
-    void* raw_block = upstream_resource->allocate(total_size_to_alloc, alignof(MemoryAllocHeader));
-    if (raw_block == nullptr)
+    void* user_ptr = upstream_resource->allocate(size, align);
+    if (user_ptr == nullptr)
     {
         throw std::bad_alloc();
     }
 
-    // 일단 유저 데이터 위치를 계산
-    void* user_ptr = static_cast<uint8*>(raw_block) + HEADER_SIZE;
-    size_t space = size + align - 1;
-
-    // user_ptr의 위치를 정렬 기준에 맞게 이동
-    std::align(align, size, user_ptr, space);
-
-    // 이렇게 이동한 user_ptr에서 HEADER_SIZE를 빼서 실제 헤더 위치를 계산
-    MemoryAllocHeader* header = reinterpret_cast<MemoryAllocHeader*>(static_cast<uint8*>(user_ptr) - HEADER_SIZE);
-
-    // 헤더가 실제 할당된 블럭으로부터 얼만큼 떨어져 있는지 계산
-    const uint8 padding = static_cast<uint8>(
-        reinterpret_cast<uintptr_t>(header) - reinterpret_cast<uintptr_t>(raw_block)
-    );
-
-    // 유저가 할당한 메모리 크기와 패딩을 기록하고 헤더 생성자 호출
-    std::construct_at(header, size, padding);
-
     // 메모리 추적
-    MemoryTracker::TrackAllocation(header, size);
+    MemoryTracker::TrackAllocation(user_ptr);
 
     return user_ptr;
 }
@@ -67,27 +42,11 @@ void TrackedMemoryResource::do_deallocate(
         return;
     }
 
-    // user_ptr로 부터 헤더 위치 계산
-    const MemoryAllocHeader* header = reinterpret_cast<const MemoryAllocHeader*>(
-        static_cast<const uint8*>(ptr) - HEADER_SIZE
-    );
-
     // 메모리 추적 해제
-    MemoryTracker::TrackDeallocation(header);
-
-    // 헤더 소멸자 호출
-    std::destroy_at(header);
-
-    // 헤더에서 패딩을 이용하여 실제 할당된 메모리 블럭 계산
-    const void* raw_block = reinterpret_cast<const uint8*>(header) - header->padding;
+    MemoryTracker::TrackDeallocation(ptr);
 
     // 할당 해제
-    const size_t total_size_to_alloc = HEADER_SIZE + header->alloc_size + align - 1;
-    upstream_resource->deallocate(
-        const_cast<void*>(raw_block),
-        total_size_to_alloc,
-        alignof(MemoryAllocHeader)
-    );
+    upstream_resource->deallocate(ptr, size, align);
 }
 
 bool TrackedMemoryResource::do_is_equal(const memory_resource& other) const noexcept
