28398deb51442b731d2aa3426710d9f793c4e131
gudtldn <gudtldn1234@gmail.com>
2025-09-20 20:51:50 +0900

    feat: DEBUG/RELEASE 매크로 정의 추가

---
+++ Engine/EngineCore/Source/Utility/Utility.h
@@ -0,0 +1,14 @@
+﻿// ReSharper disable CppClangTidyClangDiagnosticUnusedMacros
+#pragma once
+
+#if defined(DEBUG) || defined(_DEBUG)
+#define DEBUG_EXPRESION(x) x
+#else
+#define DEBUG_EXPRESION(x)
+#endif
+
+#if defined(NDEBUG)
+#define RELEASE_EXPRESION(x) x
+#else
+#define RELEASE_EXPRESION(x)
+#endif
