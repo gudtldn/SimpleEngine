f5fe1136fd92720c8dc1834293df14c849dcef3a
gudtldn <gudtldn1234@gmail.com>
2025-09-30 15:10:54 +0900

    refactor: EventWaitHandle 추가 및 코루틴 동작 개선

--- Engine/EngineCore/Source/Core/Concurrency/Coroutine/Awaitables.cpp
+++ Engine/EngineCore/Source/Core/Concurrency/Coroutine/Awaitables.cpp
@@ -40,4 +40,59 @@ void SwitchToMainThread::await_suspend(std::coroutine_handle<> handle) const
 void SwitchToMainThread::await_resume() const noexcept
 {
 }
+
+bool EventWaitHandle::Awaiter::await_ready() const noexcept
+{
+    return event.is_set.load(std::memory_order_acquire);
+}
+
+bool EventWaitHandle::Awaiter::await_suspend(std::coroutine_handle<> handle) noexcept
+{
+    continuation = handle;
+    {
+        std::scoped_lock lock(event.mutex);
+
+        if (event.is_set.load(std::memory_order_relaxed))
+        {
+            // suspend 하려는 순간 다른 스레드가 Set 해버린 경우
+            return false;
+        }
+
+        // 대기열에 추가
+        this->next = event.waiters_head;
+        event.waiters_head = this;
+    }
+    return true;
+}
+
+void EventWaitHandle::Awaiter::await_resume() const noexcept
+{
+}
+
+EventWaitHandle::Awaiter EventWaitHandle::Wait() noexcept
+{
+    return { *this };
+}
+
+void EventWaitHandle::Set() noexcept
+{
+    if (is_set.exchange(true, std::memory_order_acq_rel))
+    {
+        return;
+    }
+
+    const Awaiter* waiters_to_resume;
+    {
+        std::scoped_lock lock(mutex);
+        waiters_to_resume = std::exchange(waiters_head, nullptr);
+    }
+
+    // 모든 대기열 재개
+    while (waiters_to_resume)
+    {
+        const Awaiter* next = waiters_to_resume->next;
+        waiters_to_resume->continuation.resume();
+        waiters_to_resume = next;
+    }
+}
 }
