baa5d27854046cc8988bec54ba94477ef644f93a
gudtldn <gudtldn1234@gmail.com>
2025-10-03 00:50:08 +0900

    test: ECS 시스템 테스트 케이스 추가

---
+++ Tests/EngineTest/TestCases/ECS_Test.cpp
@@ -0,0 +1,27 @@
+﻿#include "doctest.h"
+#include "tracy/Tracy.hpp"
+
+import std;
+import SE.Prelude;
+import SE.Components;
+
+
+TEST_SUITE("SimpleEngine.Core:ECS")
+{
+using namespace se::core;
+using namespace se::core::ecs;
+
+World world;
+
+
+TEST_CASE("ECS System Test")
+{
+    world.CreateEntity()
+         .AddComponent<TransformComponent>()
+         .AddComponent<MeshHandleComponent>();
+
+    world.AddSystem([](Query<TransformComponent&, With<>, Without<>> query)
+    {
+    });
+}
+}
