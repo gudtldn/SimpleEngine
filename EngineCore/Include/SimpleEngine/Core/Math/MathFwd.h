8423c727decbd4e184c81b54a65f1f050526556b
gudtldn <gudtldn1234@gmail.com>
2025-10-16 15:48:17 +0900

    refactor: Math 모듈을 MathFwd와 Math 헤더로 분리 및 재구조화

---
+++ EngineCore/Include/SimpleEngine/Core/Math/MathFwd.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "SimpleEngine/Traits/TypeTraits.h"
+
+
+namespace se::math
+{
+template <traits::FloatingType T>
+struct Matrix4x4Impl;
+
+template <traits::FloatingType T>
+struct RotatorImpl;
+
+template <traits::FloatingType T>
+struct QuaternionImpl;
+
+template <traits::FloatingType T>
+struct Vector2Impl;
+
+template <traits::FloatingType T>
+struct Vector3Impl;
+
+template <traits::FloatingType T>
+struct Vector4Impl;
+}
