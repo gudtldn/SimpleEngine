d833855c973a17b72ee73a181b44dd8a68c7c435
gudtldn <gudtldn1234@gmail.com>
2025-10-16 16:12:07 +0900

    refactor: EventDispatcher 모듈 헤더/소스 파일로 분리 및 재구조화
    - PlatformSubsystem 내 PlatformEventDispatcher 로직 제거 및 새로운 위치로 이동
    - std 모듈 import 제거 및 관련 include 파일로 대체
    - SubscriptionHandle 및 EventDispatcher 클래스 함수 가시성 명시 (SE_CORE_API 추가)
    - SubscriptionHandle 관련 std::hash 특수화 구현 위치 재조정
    - 코드 가독성 및 유지보수성 개선 (std::ranges 활용 및 반복자 간소화)

--- EngineCore/Source/Core/Event/EventDispatcher.cpp
+++ EngineCore/Source/Core/Event/EventDispatcher.cpp
@@ -1,8 +1,40 @@
-﻿module SE.Subsystems.PlatformSubsystem;
-import :PlatformEventDispatcher;
+﻿#include "Core/Event/EventDispatcher.h"
+#include <ranges>
 
 
-SubscriptionHandle PlatformEventDispatcher::Subscribe(EventPriority priority, EventCallback callback)
+namespace se::core::event
+{
+SubscriptionHandle::SubscriptionHandle()
+    : handle_id(0)
+{
+}
+
+SubscriptionHandle::SubscriptionHandle(uint64 in_handle_id)
+    : handle_id(in_handle_id)
+{
+}
+
+uint64 SubscriptionHandle::GenerateNewID()
+{
+    static std::atomic<uint64> next_handle_id = 1;
+    uint64 result = next_handle_id.fetch_add(1, std::memory_order_relaxed);
+
+    // Overflow가 발생했다면
+    if (result == 0)
+    {
+        // 한번 더 더하기
+        result = next_handle_id.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    return result;
+}
+
+SubscriptionHandle SubscriptionHandle::CreateHandle()
+{
+    return SubscriptionHandle{ GenerateNewID() };
+}
+
+SubscriptionHandle EventDispatcher::Subscribe(EventPriority priority, EventCallback callback)
 {
     const SubscriptionHandle handle = SubscriptionHandle::CreateHandle();
     priority_map[priority].push_back(handle);
@@ -13,7 +45,7 @@ SubscriptionHandle PlatformEventDispatcher::Subscribe(EventPriority priority, Ev
     return handle;
 }
 
-void PlatformEventDispatcher::Unsubscribe(SubscriptionHandle handle)
+void EventDispatcher::Unsubscribe(SubscriptionHandle handle)
 {
     // 유효하지 않은 Handle이면 return
     if (!handle.IsValid() || !subscriptions.contains(handle))
@@ -32,7 +64,7 @@ void PlatformEventDispatcher::Unsubscribe(SubscriptionHandle handle)
     }
 }
 
-void PlatformEventDispatcher::Dispatch(PlatformEvent& event)
+void EventDispatcher::Dispatch(PlatformEvent& event)
 {
     for (const auto& handle_vector : priority_map | std::views::values)
     {
@@ -51,3 +83,13 @@ void PlatformEventDispatcher::Dispatch(PlatformEvent& event)
         }
     }
 }
+}
+
+// Specialization for SubscriptionHandle
+using se::core::event::SubscriptionHandle;
+
+size_t std::hash<SubscriptionHandle>::operator()(const SubscriptionHandle& handle) const noexcept
+{
+    return std::hash<uint64>{}(handle.GetID());
+}
+
