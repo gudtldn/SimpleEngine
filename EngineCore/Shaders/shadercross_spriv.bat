3fe8b998ff7af7ad0b9881d94db3e2ac68e34235
gudtldn <gudtldn1234@gmail.com>
2025-10-16 14:32:29 +0900

    feat: EngineCore 및 Editor CMake 설정 추가, 셰이더 스크립트 작성

--- EngineCore/Shaders/shadercross_spriv.bat
+++ EngineCore/Shaders/shadercross_spriv.bat
@@ -1,18 +1,18 @@
 @echo off
 setlocal
 
-set SHADERCROSS_EXE=..\ThirdParty\SDL3_shadercross\bin\shadercross.exe
+set SHADERCROSS_EXE=..\..\..\cmake-build-release-visual-studio\ThirdParty\SDL_shadercross\shadercross.exe
 set OUTPUT_DIR=Compiled
 
 if not exist %OUTPUT_DIR% (
     mkdir %OUTPUT_DIR%
 )
 
-echo Compiling HLSL shaders to DXIL...
+echo Compiling HLSL shaders to SPIRV...
 
 for %%f in (*.vert.hlsl) do (
     echo   Compiling %%f...
-    %SHADERCROSS_EXE% "%%f" -d DXIL -t vertex -o "%OUTPUT_DIR%\%%~nf.dxil"
+    %SHADERCROSS_EXE% "%%f" -d SPIRV -t vertex -o "%OUTPUT_DIR%\%%~nf.spv"
     if errorlevel 1 (
         echo ERROR: Failed to compile %%f
         pause
@@ -21,7 +21,7 @@ for %%f in (*.vert.hlsl) do (
 
 for %%f in (*.frag.hlsl) do (
     echo   Compiling %%f...
-    %SHADERCROSS_EXE% "%%f" -d DXIL -t fragment -o "%OUTPUT_DIR%\%%~nf.dxil"
+    %SHADERCROSS_EXE% "%%f" -d SPIRV -t fragment -o "%OUTPUT_DIR%\%%~nf.spv"
     if errorlevel 1 (
         echo ERROR: Failed to compile %%f
         pause
@@ -30,7 +30,7 @@ for %%f in (*.frag.hlsl) do (
 
 for %%f in (*.comp.hlsl) do (
     echo   Compiling %%f...
-    %SHADERCROSS_EXE% "%%f" -d DXIL -t compute -o "%OUTPUT_DIR%\%%~nf.dxil"
+    %SHADERCROSS_EXE% "%%f" -d SPIRV -t compute -o "%OUTPUT_DIR%\%%~nf.spv"
     if errorlevel 1 (
         echo ERROR: Failed to compile %%f
         pause
