ba2be499524f4d85ac67def3ac0638a9fbca8553
gudtldn <gudtldn1234@gmail.com>
2025-10-21 15:25:36 +0900

    style: UI 백버퍼 클리어 컬러 값 수정

--- Editor/Source/Rendering/EditorUIPass.cpp
+++ Editor/Source/Rendering/EditorUIPass.cpp
@@ -27,7 +27,7 @@ void EditorUIPass::Execute(const RGExecutionContext& context)
         .texture = back_buffer,
         .mip_level = 0,
         .layer_or_depth_plane = 0,
-        .clear_color = { 0.25f, 0.25f, 0.25f, 1.0f },
+        .clear_color = { 0.2f, 0.2f, 0.2f, 1.0f },
         .load_op = SDL_GPU_LOADOP_CLEAR,
         .store_op = SDL_GPU_STOREOP_STORE,
         .cycle = false,
