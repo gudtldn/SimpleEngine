9241e6e002834e339eda96d6c4c90f58826c33d1
gudtldn <gudtldn1234@gmail.com>
2025-10-24 21:47:56 +0900

    refactor: ReflectionRegistry를 TypeRegistry로 대체하여 구조 개선

---
+++ EngineCore/Include/SimpleEngine/Reflection/TypeRegistry.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <unordered_map>
+
+#include "Core/Types/StringName.h"
+#include "SimpleEngine/Core/Containers/Optional.h"
+#include "SimpleEngine/Reflection/Meta.h"
+
+
+namespace se::reflection
+{
+class SE_CORE_API TypeRegistry
+{
+    TypeRegistry() = default;
+
+public:
+    ~TypeRegistry() = default;
+
+    TypeRegistry(const TypeRegistry&) = delete;
+    TypeRegistry& operator=(const TypeRegistry&) = delete;
+    TypeRegistry(TypeRegistry&&) = delete;
+    TypeRegistry& operator=(TypeRegistry&&) = delete;
+
+    static TypeRegistry& GetInstance();
+
+public:
+    void RegisterType(TypeInfo&& type_info);
+
+    [[nodiscard]] Optional<const TypeInfo&> Find(const TypeId& type_id) const;
+    [[nodiscard]] Optional<const TypeInfo&> Find(const StringName& type_name) const;
+    [[nodiscard]] const auto& GetAllTypes() const { return type_map; }
+
+private:
+    std::unordered_map<StringName, TypeId> name_map;
+    std::unordered_map<TypeId, TypeInfo> type_map;
+};
+}
---
+++ EngineCore/Source/Reflection/TypeRegistry.cpp
@@ -0,0 +1,35 @@
+﻿#include "Reflection/TypeRegistry.h"
+
+
+namespace se::reflection
+{
+TypeRegistry& TypeRegistry::GetInstance()
+{
+    static TypeRegistry instance;
+    return instance;
+}
+
+void TypeRegistry::RegisterType(TypeInfo&& type_info)
+{
+    name_map[type_info.name] = type_info.type_id;
+    type_map[type_info.type_id] = std::move(type_info);
+}
+
+Optional<const TypeInfo&> TypeRegistry::Find(const TypeId& type_id) const
+{
+    if (const auto it = type_map.find(type_id); it != type_map.end())
+    {
+        return it->second;
+    }
+    return std::nullopt;
+}
+
+Optional<const TypeInfo&> TypeRegistry::Find(const StringName& type_name) const
+{
+    if (const auto it = name_map.find(type_name); it != name_map.end())
+    {
+        return Find(it->second);
+    }
+    return std::nullopt;
+}
+}
