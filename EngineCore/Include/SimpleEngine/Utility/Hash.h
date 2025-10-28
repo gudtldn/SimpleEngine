6efc657ec070860f077bf444d13693b6a5aebe8f
gudtldn <gudtldn1234@gmail.com>
2025-10-28 14:40:11 +0900

    refactor: FNV_Hash 함수 구조 개선 및 불필요한 중복 코드 제거

--- EngineCore/Include/SimpleEngine/Utility/Hash.h
+++ EngineCore/Include/SimpleEngine/Utility/Hash.h
@@ -7,27 +7,50 @@
 
 namespace se::utility
 {
+namespace details
+{
+constexpr uint64 DefaultFNVHash = 0xcbf29ce484222325ULL;
+
 /**
  * FNV-1a 해시 알고리즘을 사용한 문자열 해싱 함수
  * @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
  */
-template <typename StringType>
-    requires requires { typename StringType::value_type; }
-constexpr uint64 FNV_Hash(const StringType& in_str) noexcept
+template <typename CharType>
+constexpr uint64 FNV_Hash_Impl(std::basic_string_view<CharType> view) noexcept
 {
-    using CharType = StringType::value_type;
     static_assert(sizeof(CharType) == 1, "Only 1-byte character types are supported");
 
-    std::basic_string_view<CharType> sv{ in_str };
-
-    uint64 hash = 0xcbf29ce484222325ULL; // FNV_offset_basis
-    for (const CharType c : sv)
+    uint64 hash = DefaultFNVHash; // FNV_offset_basis
+    for (const CharType c : view)
     {
         const uint8 byte = static_cast<uint8>(c);
         hash ^= byte;
         hash *= 0x100000001b3ULL; // FNV_prime
     }
     return hash;
+}
+}
+
+template <typename StringType>
+    requires requires { typename StringType::value_type; }
+constexpr uint64 FNV_Hash(const StringType& in_str) noexcept
+{
+    using CharType = typename StringType::value_type;
+    return details::FNV_Hash_Impl(std::basic_string_view<CharType>{ in_str });
+}
+
+template <typename CharType, size_t N>
+constexpr uint64 FNV_Hash(const CharType(&str)[N]) noexcept
+{
+    if constexpr (N > 0)
+    {
+        // str의 N-1 길이만큼의 string_view 생성 (널 문자 제외)
+        return details::FNV_Hash_Impl<CharType>({ str, N - 1 });
+    }
+    else
+    {
+        return details::DefaultFNVHash;
+    }
 }
 
 template <typename T>
