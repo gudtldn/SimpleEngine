15eabd0b40678931204a33ae0f14e426ab23a101
gudtldn <gudtldn1234@gmail.com>
2025-11-02 17:46:26 +0900

    feat: ImGui 데모 패널 추가

---
+++ Editor/Source/UI/Panels/ImGuiDemoPanel.cpp
@@ -0,0 +1,17 @@
+﻿#include "UI/Panels/ImGuiDemoPanel.h"
+
+#include "imgui.h"
+
+
+namespace se::editor::ui
+{
+const char* ImGuiDemoPanel::GetName() const
+{
+    return "ImGui Demo";
+}
+
+void ImGuiDemoPanel::Draw(const EditorUIContext& context)
+{
+    ImGui::ShowDemoWindow(&is_visible);
+}
+}
---
+++ Editor/Source/UI/Panels/ImGuiDemoPanel.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "UI/Panels/IEditorPanel.h"
+
+
+namespace se::editor::ui
+{
+class ImGuiDemoPanel final : public IEditorPanel
+{
+public:
+    //~ IEditorPanel
+    [[nodiscard]] virtual const char* GetName() const override;
+    virtual void Draw(const EditorUIContext& context) override;
+    //~ IEditorPanel
+};
+}
