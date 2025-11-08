78968f89f51d596f20ba2971baa2bb54051acdc6
gudtldn <gudtldn1234@gmail.com>
2025-11-08 23:40:53 +0900

    refactor: Join 함수 구현 간소화 및 가독성 개선

--- EngineCore/Include/SimpleEngine/Utility/StringUtils.h
+++ EngineCore/Include/SimpleEngine/Utility/StringUtils.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <ranges>
 #include <string_view>
 
 #include "SimpleEngine/Core/Container/String.h"
@@ -19,42 +20,6 @@ template <
     requires std::convertible_to<std::ranges::range_value_t<Rng>, std::string_view>
 [[nodiscard]] BaseString<Allocator> Join(Rng&& range, std::string_view separator)
 {
-    using StringType = BaseString<Allocator>;
-    using RangeValueType = std::ranges::range_value_t<Rng>;
-
-    typename StringType::SizeType total_byte_len = 0;
-    typename StringType::SizeType element_count = 0;
-
-    for (const RangeValueType& element : range)
-    {
-        total_byte_len += std::string_view{ element }.size();
-        ++element_count;
-    }
-
-    // 구분자가 삽입될 횟수 = 요소 개수 - 1 (요소가 0개 또는 1개일 경우 0)
-    if (element_count > 0)
-    {
-        total_byte_len += separator.length() * (element_count - 1);
-    }
-
-    StringType result;
-    result.Reserve(total_byte_len);
-
-    bool first = true;
-    for (const RangeValueType& element : range)
-    {
-        if (!first)
-        {
-            result.Append(separator);
-        }
-        else
-        {
-            first = false;
-        }
-
-        result.Append(std::string_view{ element });
-    }
-
-    return result;
+    return BaseString<Allocator>::FromRange(range | std::views::join_with(separator));
 }
 }
