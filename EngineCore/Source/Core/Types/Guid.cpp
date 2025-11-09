e622c7aa3832fe59c8c8ff7344b192e102aae9c4
gudtldn <gudtldn1234@gmail.com>
2025-11-10 00:24:21 +0900

    refactor: Guid 관련 코드 가독성 개선 및 불필요한 포인터 연산 제거

--- EngineCore/Source/Core/Types/Guid.cpp
+++ EngineCore/Source/Core/Types/Guid.cpp
@@ -6,24 +6,24 @@
 
 namespace
 {
-const uuids::uuid* AsUuid(const Guid& guid) noexcept
+const uuids::uuid& AsUuid(const Guid& guid) noexcept
 {
     static_assert(sizeof(Guid) == sizeof(uuids::uuid));
     static_assert(alignof(Guid) == alignof(uuids::uuid));
-    return reinterpret_cast<const uuids::uuid*>(&guid);
+    return reinterpret_cast<const uuids::uuid&>(guid);
 }
 
-uuids::uuid* AsUuid(Guid& guid) noexcept
+uuids::uuid& AsUuid(Guid& guid) noexcept
 {
     static_assert(sizeof(Guid) == sizeof(uuids::uuid));
     static_assert(alignof(Guid) == alignof(uuids::uuid));
-    return reinterpret_cast<uuids::uuid*>(&guid);
+    return reinterpret_cast<uuids::uuid&>(guid);
 }
 
 Guid FromUuid(const uuids::uuid& uuid)
 {
     Guid guid;
-    *AsUuid(guid) = uuid;
+    AsUuid(guid) = uuid;
     return guid;
 }
 }
@@ -33,38 +33,38 @@ Guid Guid::None{};
 
 Guid::Guid() noexcept
 {
-    new(AsUuid(*this)) uuids::uuid{};
+    new(&AsUuid(*this)) uuids::uuid{};
 }
 
 Guid::~Guid()
 {
-    AsUuid(*this)->~uuid();
+    AsUuid(*this).~uuid();
 }
 
 Guid::Guid(const Guid& other) noexcept
 {
-    new(AsUuid(*this)) uuids::uuid{ *AsUuid(other) };
+    new(&AsUuid(*this)) uuids::uuid{ AsUuid(other) };
 }
 
 Guid& Guid::operator=(const Guid& other) noexcept
 {
     if (this != &other)
     {
-        *AsUuid(*this) = *AsUuid(other);
+        AsUuid(*this) = AsUuid(other);
     }
     return *this;
 }
 
 Guid::Guid(Guid&& other) noexcept
 {
-    new(AsUuid(*this)) uuids::uuid{ *AsUuid(other) };
+    new(&AsUuid(*this)) uuids::uuid{ AsUuid(other) };
 }
 
 Guid& Guid::operator=(Guid&& other) noexcept
 {
     if (this != &other)
     {
-        *AsUuid(*this) = *AsUuid(other);
+        AsUuid(*this) = AsUuid(other);
     }
     return *this;
 }
@@ -81,12 +81,12 @@ Guid Guid::FromString(const se::String& str)
 
 bool Guid::IsValid() const noexcept
 {
-    return !AsUuid(*this)->is_nil();
+    return !AsUuid(*this).is_nil();
 }
 
 se::String Guid::ToString() const
 {
-    return se::String{ uuids::to_string(*AsUuid(*this)) };
+    return se::String{ uuids::to_string(AsUuid(*this)) };
 }
 
 Guid::operator bool() const noexcept
@@ -96,5 +96,5 @@ Guid::operator bool() const noexcept
 
 size_t std::hash<Guid>::operator()(const Guid& guid) const noexcept
 {
-    return std::hash<uuids::uuid>{}(*AsUuid(guid));
+    return std::hash<uuids::uuid>{}(AsUuid(guid));
 }
