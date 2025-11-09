cfaa0998a448f9470ac06087fc8e3b007bd0ad00
gudtldn <gudtldn1234@gmail.com>
2025-11-10 00:10:35 +0900

    feat: Guid 타입 추가 및 관련 유틸리티 함수 구현

---
+++ EngineCore/Include/SimpleEngine/Core/Types/Guid.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <compare>
+
+#include "SimpleEngine/Core/Container/FixedArray.h"
+#include "SimpleEngine/Core/Container/String.h"
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+
+
+/**
+ * @todo docs
+ */
+class SE_CORE_API Guid
+{
+public:
+    Guid() noexcept;
+    ~Guid();
+
+    Guid(const Guid& other) noexcept;
+    Guid& operator=(const Guid& other) noexcept;
+    Guid(Guid&& other) noexcept;
+    Guid& operator=(Guid&& other) noexcept;
+
+public:
+    static Guid None;
+
+    [[nodiscard]] static Guid NewGuid();
+    [[nodiscard]] static Guid FromString(const se::String& str);
+
+public:
+    [[nodiscard]] bool IsValid() const noexcept;
+    [[nodiscard]] se::String ToString() const;
+
+public:
+    [[nodiscard]] explicit operator bool() const noexcept;
+    [[nodiscard]] bool operator==(const Guid& other) const noexcept = default;
+
+private:
+    se::FixedArray<uint8, 16> data;
+};
+
+template <>
+struct std::hash<Guid>
+{
+    size_t operator()(const Guid& guid) const noexcept;
+};
