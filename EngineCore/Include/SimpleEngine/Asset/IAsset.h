e455bae2c54aca5dd27cc147bc36fa288cf4a479
gudtldn <gudtldn1234@gmail.com>
2025-11-19 01:16:26 +0900

    wip, refactor: AssetLoader 인터페이스를 IAsset 및 IAssetLoader 기반으로 재구조화

---
+++ EngineCore/Include/SimpleEngine/Asset/IAsset.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+
+
+namespace se::asset
+{
+/**
+ * Asset을 나타내는 가장 기본적인 타입
+ */
+class SE_CORE_API IAsset
+{
+public:
+    virtual ~IAsset() = default;
+};
+}
---
+++ EngineCore/Include/SimpleEngine/Asset/Loaders/IAssetLoader.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <filesystem>
+#include <memory>
+
+#include "SimpleEngine/Asset/IAsset.h"
+#include "SimpleEngine/Core/Concurrency/Coroutine.h"
+
+
+namespace se::asset
+{
+class SE_CORE_API IAssetLoader
+{
+public:
+    virtual ~IAssetLoader() = default;
+    virtual concurrency::Task<std::shared_ptr<IAsset>> Load(const std::filesystem::path& physical_path) = 0;
+};
+}
