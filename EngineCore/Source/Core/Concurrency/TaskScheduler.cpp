8067628fbbea1a46b92930d8435f1f83414d6419
gudtldn <gudtldn1234@gmail.com>
2025-11-18 16:50:10 +0900

    refactor: TaskScheduler에 워커/IO 스레드 분리 및 Launch/Schedule 함수 추가

--- EngineCore/Source/Core/Concurrency/TaskScheduler.cpp
+++ EngineCore/Source/Core/Concurrency/TaskScheduler.cpp
@@ -1,9 +1,9 @@
-﻿#include "Core/Concurrency/TaskScheduler.h"
-
-#include <cassert>
+﻿// ReSharper disable CppMemberFunctionMayBeConst
+#include "Core/Concurrency/TaskScheduler.h"
 
 #include "Core/Concurrency/ThreadPool.h"
 #include "tracy/Tracy.hpp"
+#include "Utility/Debug.h"
 
 
 namespace se::concurrency
@@ -13,22 +13,57 @@ TaskScheduler* TaskScheduler::Instance = nullptr;
 TaskScheduler::TaskScheduler(std::thread::id in_main_thread_id)
     : main_thread_id(in_main_thread_id)
 {
-    assert(main_thread_id == std::this_thread::get_id() && "TaskScheduler instance is created on a non-main thread!");
-    assert(!Instance && "TaskScheduler instance is already created!");
+    SE_ASSERT(main_thread_id == std::this_thread::get_id(), "TaskScheduler instance is created on a non-main thread!");
+    SE_ASSERT(!Instance, "TaskScheduler instance is already created!");
 
     Instance = this;
+
+    // 메인 스레드를 제외한 실제 워커 스레드로 사용 가능한 코어 수
+    const uint32 core_count = std::thread::hardware_concurrency();
+    const uint32 worker_cores = std::max(1u, core_count - 1);
+
+    // 워커 코어를 Compute(80%) 와 IO(20%)로 분배
+    const uint32 compute_threads = std::max(1u, static_cast<uint32>(worker_cores * 0.8));
+    const uint32 io_threads = worker_cores - compute_threads;
+
+    compute_pool = std::make_unique<ThreadPool>("Worker Thread", compute_threads);
+    io_pool = std::make_unique<ThreadPool>("I/O Thread", io_threads);
 }
 
 TaskScheduler::~TaskScheduler()
 {
-    assert(Instance == this && "TaskScheduler instance is not created!");
+    SE_ASSERT(Instance == this, "TaskScheduler instance is not created!");
     Instance = nullptr;
 }
 
 TaskScheduler& TaskScheduler::Get()
 {
-    assert(Instance && "TaskScheduler instance is not initialized.");
+    SE_ASSERT(Instance, "TaskScheduler instance is not initialized.");
     return *Instance;
+}
+
+void TaskScheduler::Launch_MainThread(Task<void>&& task)
+{
+    if (const auto handle_to_resume = PrepareTaskToLaunch(std::move(task)))
+    {
+        ScheduleOnMainThread(handle_to_resume);
+    }
+}
+
+void TaskScheduler::Launch_WorkerThread(Task<void>&& task)
+{
+    if (const auto handle_to_resume = PrepareTaskToLaunch(std::move(task)))
+    {
+        ScheduleOnWorkerThread(handle_to_resume);
+    }
+}
+
+void TaskScheduler::Launch_IOThread(Task<void>&& task)
+{
+    if (const auto handle_to_resume = PrepareTaskToLaunch(std::move(task)))
+    {
+        ScheduleOnIOThread(handle_to_resume);
+    }
 }
 
 std::thread::id TaskScheduler::GetMainThreadId() const
@@ -36,15 +71,15 @@ std::thread::id TaskScheduler::GetMainThreadId() const
     return main_thread_id;
 }
 
-void TaskScheduler::Launch_MainThread(Task<void>&& task)
+Task<void>::HandleType TaskScheduler::PrepareTaskToLaunch(Task<void>&& task)
 {
+    // 유효하지 않은 handle은 return
     if (!task.handle)
     {
-        return;
+        return {};
     }
 
     const auto handle_to_resume = task.handle;
-
     {
         std::scoped_lock lock(tasks_mutex);
 
@@ -52,29 +87,30 @@ void TaskScheduler::Launch_MainThread(Task<void>&& task)
         launched_tasks.Push(std::move(task));
     }
 
-    // Task의 handle을 사용하여 코드를 실행
-    handle_to_resume.resume();
+    return handle_to_resume;
 }
 
-void TaskScheduler::Launch_WorkerThread(Task<void>&& task)
+void TaskScheduler::ScheduleOnMainThread(std::coroutine_handle<> handle)
 {
-    if (!task.handle)
-    {
-        return;
-    }
+    std::scoped_lock lock(main_thread_mutex);
+    main_thread_tasks.Push(handle);
+}
 
-    auto handle_to_resume = task.handle;
-
+void TaskScheduler::ScheduleOnWorkerThread(std::coroutine_handle<> handle)
+{
+    // compute_pool에 작업 제출
+    compute_pool->Submit([handle]
     {
-        std::scoped_lock lock(tasks_mutex);
-        // Task의 소유권을 이동시켜 수명을 연장
-        launched_tasks.Push(std::move(task));
-    }
+        handle.resume();
+    });
+}
 
-    // Task의 handle을 사용하여 코드를 실행
-    ThreadPool::SubmitTask([handle_to_resume]
+void TaskScheduler::ScheduleOnIOThread(std::coroutine_handle<> handle)
+{
+    // io_pool에 작업 제출
+    io_pool->Submit([handle]
     {
-        handle_to_resume.resume();
+        handle.resume();
     });
 }
 
@@ -88,9 +124,8 @@ void TaskScheduler::ProcessMainThreadTasks()
     }
 
     // 다시 예약된 모든 코루틴을 재개합니다.
-    while (const Optional handle_opt = tasks_to_run.Front())
+    while (const Optional handle_opt = tasks_to_run.Pop())
     {
-        tasks_to_run.Pop();
         handle_opt->resume();
     }
 
@@ -103,11 +138,5 @@ void TaskScheduler::ProcessMainThreadTasks()
             return !task.handle || task.handle.done();
         });
     }
-}
-
-void TaskScheduler::ScheduleOnMainThread(std::coroutine_handle<> handle)
-{
-    std::scoped_lock lock(main_thread_mutex);
-    main_thread_tasks.Push(handle);
 }
 }
