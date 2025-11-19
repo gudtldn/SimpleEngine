ba5c10162dae6c97e1b133d8a862fd80412f040a
gudtldn <gudtldn1234@gmail.com>
2025-11-19 23:55:03 +0900

    feat: Texture2D 로더를 에셋 서브시스템에 등록하여 이미지 로드 지원 추가

--- EngineCore/Source/Asset/AssetSubsystem.cpp
+++ EngineCore/Source/Asset/AssetSubsystem.cpp
@@ -1,5 +1,8 @@
 ﻿#include "Asset/AssetSubsystem.h"
 
+#include "Asset/Loaders/Texture2DLoader.h"
+#include "Asset/Types/Texture2D.h"
+
 using namespace se::asset;
 
 
@@ -7,6 +10,10 @@ bool AssetSubsystem::Initialize()
 {
     ConsoleLog(ELogLevel::Info, "Initializing Asset subsystem...");
     asset_manager = std::make_unique<AssetManager>();
+
+    asset_manager->RegisterLoader<Texture2D, Texture2DLoader>(".png");
+    asset_manager->RegisterLoader<Texture2D, Texture2DLoader>(".jpg");
+    asset_manager->RegisterLoader<Texture2D, Texture2DLoader>(".jpeg");
 
     return true;
 }
