b95827e9c08f5e0e99692b3d96481ac480e27027
gudtldn <gudtldn1234@gmail.com>
2025-11-19 17:27:04 +0900

    feat: 에디터에 AssetsBrowserPanel 추가 및 디렉터리 탐색 UI 구현

---
+++ Editor/Source/UI/Panels/AssetsBrowserPanel.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <filesystem>
+
+#include "IEditorPanel.h"
+
+
+namespace se::editor::ui
+{
+class AssetsBrowserPanel : public IEditorPanel
+{
+public:
+    [[nodiscard]] virtual const char* GetName() const override;
+    virtual void Draw() override;
+
+protected:
+    virtual void DrawAssetTree();
+    virtual void DrawAssetGrid();
+
+protected:
+    [[nodiscard]] static bool HasSubDirectories(const std::filesystem::path& path);
+
+    [[nodiscard]] const std::filesystem::path& GetSelectedDirPath() const noexcept { return selected_dir_path; }
+    void SetSelectedDirPath(const std::filesystem::path& new_path) noexcept { selected_dir_path = new_path; }
+
+private:
+    void RenderDirectoryTreeRecursive(const std::filesystem::path& path);
+
+private:
+    std::filesystem::path selected_dir_path;
+};
+}
