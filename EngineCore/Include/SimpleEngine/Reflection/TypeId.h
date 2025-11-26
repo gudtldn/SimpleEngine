dd4810e3a675b786f44c403ee5f77075b4fa8f17
gudtldn <gudtldn1234@gmail.com>
2025-11-26 16:35:55 +0900

    feat: TypeId 유효성 검사 및 bool 변환 연산자 추가

--- EngineCore/Include/SimpleEngine/Reflection/TypeId.h
+++ EngineCore/Include/SimpleEngine/Reflection/TypeId.h
@@ -36,14 +36,14 @@ public:
      * 해시를 이용해 TypeId를 생성합니다.
      * 만약 TypeRegistry에 없는 Hash면 빈 TypeId를 반환합니다.
      */
-    static TypeId FromHash(uint64 in_hash);
+    [[nodiscard]] static TypeId FromHash(uint64 in_hash);
 
     /**
      * 타입 이름을 이용해 TypeId를 생성합니다.
      * 만약 TypeRegistry에 없는 이름이면 빈 TypeId를 반환합니다.
      * @note 이름은 namespace 포함 타입 이름입니다. (예: se::asset::Texture2D)
      */
-    static TypeId FromName(const StringName& in_type_name);
+    [[nodiscard]] static TypeId FromName(const StringName& in_type_name);
 
 public:
     /** 타입 이름을 반환합니다. */
@@ -52,9 +52,13 @@ public:
     /** 타입 해시를 반환합니다. */
     [[nodiscard]] constexpr uint64 GetHash() const { return type_hash; }
 
+    /** TypeId가 올바른지 확인합니다. */
+    [[nodiscard]] constexpr bool IsValid() const { return type_hash != 0; }
+
 public:
-    constexpr bool operator==(const TypeId& other) const { return type_hash == other.type_hash; }
-    constexpr auto operator<=>(const TypeId& other) const { return type_hash <=> other.type_hash; }
+    [[nodiscard]] constexpr bool operator==(const TypeId& other) const { return type_hash == other.type_hash; }
+    [[nodiscard]] constexpr auto operator<=>(const TypeId& other) const { return type_hash <=> other.type_hash; }
+    [[nodiscard]] explicit constexpr operator bool() const { return IsValid(); }
 
 private:
     constexpr TypeId(std::string_view in_type_name, std::string_view in_type_hash)
