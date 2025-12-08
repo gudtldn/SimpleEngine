1ad33c653fbc83e640e65b7eb94d82c01f701b33
gudtldn <gudtldn1234@gmail.com>
2025-12-09 00:15:24 +0900

    refactor: ComponentRegistry 클래스 기본 구조 추가 및 코드 정리

---
+++ EngineCore/Include/SimpleEngine/ECS/ComponentRegistry.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "SimpleEngine/Core/Container/HashMap.h"
+#include "SimpleEngine/Core/Functional/Function.h"
+#include "SimpleEngine/ECS/World.h"
+#include "SimpleEngine/Reflection/TypeId.h"
+
+
+namespace se::ecs
+{
+/**
+ * @todo docs
+ */
+class SE_CORE_API ComponentRegistry
+{
+public:
+    using StorageFactory = Function<void(World&)>;
+
+    static ComponentRegistry& Get()
+    {
+        static ComponentRegistry instance;
+        return instance;
+    }
+
+    template <typename T>
+    static void Register()
+    {
+        Get().factories.Emplace(refl::TypeId::Get<T>(), [](World& world)
+        {
+            world.GetOrCreateStorage<T>();
+        });
+    }
+
+    bool EnsureStorage(World& world, const refl::TypeId& type_id) const
+    {
+        if (const Optional func_opt = factories.Find(type_id))
+        {
+            func_opt->Invoke(world);
+            return true;
+        }
+        return false;
+    }
+
+private:
+    HashMap<refl::TypeId, StorageFactory> factories;
+};
+}  // namespace se::ecs
