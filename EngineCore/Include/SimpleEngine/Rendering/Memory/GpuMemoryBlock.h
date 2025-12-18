4770156096704615fd45f054579391ca4d6d4179
gudtldn <gudtldn1234@gmail.com>
2025-12-18 23:15:56 +0900

    wip, feat: GPU 메모리 관리를 위한 GpuMemoryBlock 클래스 추가

---
+++ EngineCore/Include/SimpleEngine/Rendering/Memory/GpuMemoryBlock.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "SimpleEngine/Core/Container/Array.h"
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+#include "SimpleEngine/Rendering/Memory/GpuBufferSlice.h"
+
+#include "SDL3/SDL_gpu.h"
+
+
+namespace se::rendering
+{
+/**
+ * GPU VRAM의 거대한 단일 할당 블록을 관리하는 클래스
+ * 하나의 SDL_GPUBuffer를 소유하며, 이를 작은 Slice로 나누어 제공합니다.
+ */
+class SE_CORE_API GpuMemoryBlock
+{
+public:
+    GpuMemoryBlock(SDL_GPUDevice* in_device, uint32 in_size, SDL_GPUBufferUsageFlags in_usage);
+    ~GpuMemoryBlock();
+
+    // 이동만 허용
+    GpuMemoryBlock(const GpuMemoryBlock&) = delete;
+    GpuMemoryBlock& operator=(const GpuMemoryBlock&) = delete;
+    GpuMemoryBlock(GpuMemoryBlock&& other) noexcept;
+    GpuMemoryBlock& operator=(GpuMemoryBlock&& other) noexcept;
+
+public:
+    [[nodiscard]] bool AllocateSlice(uint32 in_size, uint32 in_alignment, GpuBufferSlice& out_slice);
+    [[nodiscard]] SDL_GPUBuffer* GetNativeBuffer() const noexcept { return buffer; }
+
+private:
+    SDL_GPUDevice* device;
+    SDL_GPUBuffer* buffer; // GPU Buffer 리소스
+
+    uint32 total_size;  // 버퍼 크기
+    uint32 used_offset; // 버퍼를 할당할 수 있는 시작 오프셋
+};
+}  // namespace se::rendering
---
+++ EngineCore/Source/Rendering/Memory/GpuMemoryBlock.cpp
@@ -0,0 +1,81 @@
+﻿#include "Rendering/Memory/GpuMemoryBlock.h"
+
+#include "Utility/Common.h"
+
+
+namespace se::rendering
+{
+GpuMemoryBlock::GpuMemoryBlock(SDL_GPUDevice* in_device, uint32 in_size, SDL_GPUBufferUsageFlags in_usage)
+    : device(in_device)
+    , total_size(in_size)
+    , used_offset(0)
+{
+    // const SDL_PropertiesID buffer_props = SDL_CreateProperties();
+    // SDL_SetStringProperty(buffer_props, SDL_PROP_GPU_BUFFER_CREATE_NAME_STRING, );
+
+    const SDL_GPUBufferCreateInfo buffer_info = {
+        .usage = in_usage,
+        .size = in_size,
+        // .props = buffer_props
+    };
+    buffer = SDL_CreateGPUBuffer(device, &buffer_info);
+
+    // SDL_DestroyProperties(buffer_props);
+}
+
+GpuMemoryBlock::~GpuMemoryBlock()
+{
+    SDL_ReleaseGPUBuffer(device, buffer);
+}
+
+GpuMemoryBlock::GpuMemoryBlock(GpuMemoryBlock&& other) noexcept
+{
+    *this = std::move(other);
+}
+
+GpuMemoryBlock& GpuMemoryBlock::operator=(GpuMemoryBlock&& other) noexcept
+{
+    if (this != &other)
+    {
+        SDL_ReleaseGPUBuffer(device, buffer);
+
+        static_assert(
+            utility::AlignedSize<alignof(GpuMemoryBlock)>(
+                sizeof(device)   // NOLINT(*-sizeof-expression)
+                + sizeof(buffer) // NOLINT(*-sizeof-expression)
+                + sizeof(total_size)
+                + sizeof(used_offset)
+            ) == sizeof(GpuMemoryBlock),
+            "GpuMemoryBlock size mismatch"
+        );
+
+        device = std::exchange(other.device, nullptr);
+        buffer = std::exchange(other.buffer, nullptr);
+        total_size = std::exchange(other.total_size, 0);
+        used_offset = std::exchange(other.used_offset, 0);
+    }
+    return *this;
+}
+
+bool GpuMemoryBlock::AllocateSlice(uint32 in_size, uint32 in_alignment, GpuBufferSlice& out_slice)
+{
+    const uint32 aligned_offset = static_cast<uint32>(utility::AlignedSize(used_offset, in_alignment));
+
+    // Buffer의 남은 공간이 부족한지?
+    if (aligned_offset + in_size > total_size)
+    {
+        return false;
+    }
+
+    out_slice.buffer = buffer;
+    out_slice.offset = aligned_offset;
+    out_slice.size = in_size;
+
+    // index_offset는 여기서 알 수 없으므로, GpuResourceManager가 설정
+    out_slice.index_offset = 0;
+
+    // 다음 할당을 위해 Offset 이동
+    used_offset = aligned_offset + in_size;
+    return true;
+}
+}  // namespace se::rendering
