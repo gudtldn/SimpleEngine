323db0b763a2cebd53a7c880176d2aeacaccf6f3
gudtldn <gudtldn1234@gmail.com>
2026-01-22 23:08:34 +0900

    docs: StaticMesh 구조 변경 아이디어 관련 주석 추가

--- EngineCore/Include/SimpleEngine/Asset/Types/MeshTypes.h
+++ EngineCore/Include/SimpleEngine/Asset/Types/MeshTypes.h
@@ -12,6 +12,23 @@ namespace se::asset
  */
 struct SE_CORE_API StaticMesh : IAsset
 {
+    // 추후 Depth Prepass를 위한 Position정보와 나머지 Vertex정보를 분리해서
+    // 하이브리드 SoA 방식으로 구조를 변경해 볼 수도 있음.
+    // (GPU Buffer를 positions +(pad) attributes +(pad) indices로 할당해서, Buffer를 2개로 나눠 Slot0, 1에 할당)
+    //
+    // struct gfx::VertexAttributes
+    // {
+    //     Vector3f normal;
+    //     Vector2f tex_coord;
+    //     Vector4f tangent;
+    // }
+    //
+    // SE_PROPERTY(=meta::ReadOnly)
+    // Array<Vector3f> positions;
+    //
+    // SE_PROPERTY(=meta::ReadOnly)
+    // Array<gfx::VertexAttributes> attributes;
+
     SE_PROPERTY(=meta::ReadOnly)
     Array<gfx::Vertex> vertices;
 
