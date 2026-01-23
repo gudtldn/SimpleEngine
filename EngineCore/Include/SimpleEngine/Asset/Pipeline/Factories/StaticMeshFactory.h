20fa7a98c03770ea3455410d4d0ff9cb3931129b
gudtldn <gudtldn1234@gmail.com>
2026-01-23 18:49:34 +0900

    feat: StaticMeshFactory 클래스 초기 구현 추가 및 StaticMesh 생성 로직 작성

---
+++ EngineCore/Include/SimpleEngine/Asset/Pipeline/Factories/StaticMeshFactory.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "SimpleEngine/Asset/Pipeline/Factories/IPipelineFactory.h"
+
+
+namespace se::asset
+{
+/**
+ * @todo docs
+ */
+class SE_CORE_API StaticMeshFactory : public IPipelineFactory
+{
+public:
+    [[nodiscard]] virtual bool CanCreateAsset(const PipelineBaseNode* node) const override;
+    virtual std::shared_ptr<IAsset> CreateAsset(PipelineBaseNode* node, const PipelineImportContext& context) override;
+
+    [[nodiscard]] virtual refl::TypeId GetAssetType() const override;
+};
+}
---
+++ EngineCore/Source/Asset/Pipeline/Factories/StaticMeshFactory.cpp
@@ -0,0 +1,43 @@
+﻿#include "Asset/Pipeline/Factories/StaticMeshFactory.h"
+
+#include "Asset/Pipeline/Nodes/StaticMeshPipelineNode.h"
+#include "Asset/Types/MeshTypes.h"
+
+
+namespace se::asset
+{
+bool StaticMeshFactory::CanCreateAsset(const PipelineBaseNode* node) const
+{
+    return node->GetTypeId() == refl::TypeId::Get<StaticMeshPipelineNode>();
+}
+
+std::shared_ptr<IAsset> StaticMeshFactory::CreateAsset(
+    PipelineBaseNode* node,
+    const PipelineImportContext& context
+)
+{
+    StaticMeshPipelineNode* mesh_node = static_cast<StaticMeshPipelineNode*>(node);
+
+    auto static_mesh = std::make_shared<StaticMesh>();
+    static_mesh->vertices = std::move(mesh_node->vertices);
+    static_mesh->indices = std::move(mesh_node->indices);
+    static_mesh->sections = std::move(mesh_node->sections);
+
+    // TODO: AABB 계산
+    // CalculateBounds(static_mesh);
+
+    // TODO: 머티리얼 연결
+    // for (const auto& section : static_mesh->sections)
+    // {
+    //      // context.GetCreatedAsset(...)를 통해 머티리얼 에셋을 찾아서 연결
+    // }
+    (void)context;
+
+    return static_mesh;
+}
+
+refl::TypeId StaticMeshFactory::GetAssetType() const
+{
+    return refl::TypeId::Get<StaticMesh>();
+}
+}
