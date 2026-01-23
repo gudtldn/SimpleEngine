be0ca4f1ab2c60eb444c2b9dd3d5d012d2ceb640
gudtldn <gudtldn1234@gmail.com>
2026-01-23 13:04:44 +0900

    refactor: PipelineTranslator 메서드 파라미터 타입을 String → std::filesystem::path로 변경

--- EngineCore/Include/SimpleEngine/Asset/Pipeline/Translators/IPipelineTranslator.h
+++ EngineCore/Include/SimpleEngine/Asset/Pipeline/Translators/IPipelineTranslator.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include <filesystem>
+
 #include "SimpleEngine/Asset/Pipeline/PipelineNodeContainer.h"
 
 
@@ -12,10 +14,10 @@ class SE_CORE_API IPipelineTranslator
 public:
     virtual ~IPipelineTranslator() = default;
 
-    /** Translator가 지원하는 파일 확장자인지를 반환합니다. */
-    [[nodiscard]] virtual bool CanTranslate(const String& file_extension) const = 0;
+    /** Translator가 지원하는 파일 확장자인지 확인합니다. */
+    [[nodiscard]] virtual bool CanTranslate(const std::filesystem::path& file_extension) const = 0;
 
     /** 파일을 읽고 Container에 Node를 채웁니다. */
-    virtual void Translate(const String& file_path, PipelineNodeContainer& out_container) = 0;
+    virtual void Translate(const std::filesystem::path& file_path, PipelineNodeContainer& out_container) = 0;
 };
 }  // namespace se::asset
