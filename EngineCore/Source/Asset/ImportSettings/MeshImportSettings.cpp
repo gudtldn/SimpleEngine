9464dc28aa72523e6a6b4065c1be23d43c92ec61
gudtldn <gudtldn1234@gmail.com>
2026-01-25 22:46:43 +0900

    fix: MeshImportSettings의 global_scale 최소값 조정

--- EngineCore/Source/Asset/ImportSettings/MeshImportSettings.cpp
+++ EngineCore/Source/Asset/ImportSettings/MeshImportSettings.cpp
@@ -7,7 +7,7 @@ namespace se::asset
 SE_BEGIN_REFLECT(MeshImportSettings)
     SE_REFLECT_PROPERTY(combine_meshes, meta::Edit, meta::DisplayName("Combine Meshes"))
     SE_REFLECT_PROPERTY(apply_transform, meta::Edit, meta::DisplayName("Apply Transform"))
-    SE_REFLECT_PROPERTY(global_scale, meta::Edit, meta::Range(0.1f, 1000.0f), meta::DisplayName("Global Scale"))
+    SE_REFLECT_PROPERTY(global_scale, meta::Edit, meta::Range(0.01f, 1000.0f), meta::DisplayName("Global Scale"))
 SE_END_REFLECT(MeshImportSettings)
 
 void MeshImportSettings::Serialize(core::Archive& ar)
