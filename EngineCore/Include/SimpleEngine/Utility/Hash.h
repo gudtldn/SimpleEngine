34c83e00c43b090e4f5bacdff7b9b0413652bda8
gudtldn <gudtldn1234@gmail.com>
2026-01-27 00:31:55 +0900

    refactor: FNV_Hash 함수 개선 및 코드 구조 정리

--- EngineCore/Include/SimpleEngine/Utility/Hash.h
+++ EngineCore/Include/SimpleEngine/Utility/Hash.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <concepts>
 #include <string_view>
 #include "SimpleEngine/Core/HAL/PlatformTypes.h"
 
@@ -8,59 +9,54 @@ namespace se::utility
 namespace details
 {
 constexpr uint64 DefaultFNVHash = 0xcbf29ce484222325ULL;
+constexpr uint64 FNV_Prime = 0x100000001b3ULL;
 
 /**
  * FNV-1a 해시 알고리즘을 사용한 문자열 해싱 함수
  * @see https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
  */
-template <typename CharType>
-constexpr uint64 FNV_Hash_Impl(std::basic_string_view<CharType> view) noexcept
+template <typename CharType, typename TransformFunc>
+constexpr uint64 FNV_Hash_Impl(std::basic_string_view<CharType> view, TransformFunc transform) noexcept
 {
     static_assert(sizeof(CharType) == 1, "Only 1-byte character types are supported");
 
     uint64 hash = DefaultFNVHash; // FNV_offset_basis
     for (const CharType c : view)
     {
-        const uint8 byte = static_cast<uint8>(c);
-        hash ^= byte;
-        hash *= 0x100000001b3ULL; // FNV_prime
+        hash ^= static_cast<uint8>(transform(c));
+        hash *= FNV_Prime;
     }
     return hash;
 }
-}
+}  // namespace details
 
-template <typename StringType>
-    requires requires { typename StringType::value_type; }
-constexpr uint64 FNV_Hash(const StringType& in_str) noexcept
+template <typename T>
+    requires std::is_convertible_v<T, std::string_view>
+constexpr uint64 FNV_Hash(const T& str) noexcept
 {
-    using CharType = StringType::value_type;
-    return details::FNV_Hash_Impl(std::basic_string_view<CharType>{ in_str });
-}
-
-template <typename CharType, usize N>
-constexpr uint64 FNV_Hash(const CharType(&str)[N]) noexcept
-{
-    if constexpr (N > 0)
-    {
-        // str의 N-1 길이만큼의 string_view 생성 (널 문자 제외)
-        return details::FNV_Hash_Impl<CharType>({ str, N - 1 });
-    }
-    else
-    {
-        return details::DefaultFNVHash;
-    }
+    return details::FNV_Hash_Impl(std::string_view{ str }, [](auto c) { return c; });
 }
 
 template <typename T>
-void HashCombine(usize& seed, const T& v)
+    requires std::is_convertible_v<T, std::string_view>
+constexpr uint64 FNV_Hash_CaseInsensitive(const T& str) noexcept
 {
-    std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    return details::FNV_Hash_Impl(
+        std::string_view{ str },
+        [](auto c)
+        {
+            return ('A' <= c && c <= 'Z') ? c | 0x20 : c;
+        }
+    );
 }
 
 template <typename... Ts>
 void HashCombine(usize& seed, const Ts&... values)
 {
-    (HashCombine(seed, values), ...);
+    const auto combine_one = [&]<typename T>(const T& v)
+    {
+        seed ^= std::hash<std::decay_t<T>>{}(v) + 0x9e3779b97f4a7c15 + (seed << 6) + (seed >> 2);
+    };
+    (combine_one(values), ...);
 }
-}
+}  // namespace se::utility
