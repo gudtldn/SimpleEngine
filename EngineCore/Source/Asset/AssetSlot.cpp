8ef7823a48ccf22d057043ea226f9eb999c89c6f
gudtldn <gudtldn1234@gmail.com>
2026-02-01 19:56:08 +0900

    feat: Asset을 관리하는 AssetSlot 클래스 추가

---
+++ EngineCore/Source/Asset/AssetSlot.cpp
@@ -0,0 +1,42 @@
+﻿#include "Asset/AssetSlot.h"
+
+
+namespace se::asset
+{
+AssetSlot::AssetSlot(Path path, const TypeId& type_id)
+    : asset_type(type_id)
+    , source_path(std::move(path))
+    , loading_state(ELoadingState::Unloaded)
+{
+}
+
+std::shared_ptr<IAsset> AssetSlot::GetAsset() const
+{
+    std::shared_lock lock(mutex);
+    return asset;
+}
+
+void AssetSlot::SetAsset(std::shared_ptr<IAsset> new_asset, ELoadingState new_state)
+{
+    std::unique_lock lock(mutex);
+    asset = std::move(new_asset);
+    SetState(new_state);
+}
+
+ELoadingState AssetSlot::GetState() const
+{
+    return loading_state.load(std::memory_order_acquire);
+}
+
+void AssetSlot::SetState(ELoadingState state)
+{
+    loading_state.store(state, std::memory_order_release);
+}
+
+void AssetSlot::Invalidate()
+{
+    std::unique_lock lock(mutex);
+    asset.reset();
+    SetState(ELoadingState::Unloaded);
+}
+} // namespace se::asset
