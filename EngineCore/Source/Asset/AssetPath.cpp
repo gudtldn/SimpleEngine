4513da1662e60f6b15b0c9819cea2f66b90347ae
gudtldn <gudtldn1234@gmail.com>
2026-02-04 22:47:08 +0900

    feat: Sub-Asset을 받을 수 있는 AssetPath 구현

---
+++ EngineCore/Source/Asset/AssetPath.cpp
@@ -0,0 +1,35 @@
+﻿#include "Asset/AssetPath.h"
+
+#include <utility>
+
+
+namespace se::asset
+{
+AssetPath::AssetPath(StringView full_path_str)
+{
+    const Optional separator_pos_opt = full_path_str.Find('#');
+    if (!separator_pos_opt)
+    {
+        file_path = full_path_str;
+        return;
+    }
+
+    file_path = full_path_str.Substr(0, *separator_pos_opt);
+    sub_asset_name = full_path_str.Substr(*separator_pos_opt + 1);
+}
+
+AssetPath::AssetPath(const Path& in_file_path, StringView in_sub_asset_name)
+    : file_path(in_file_path)
+    , sub_asset_name(in_sub_asset_name)
+{
+}
+
+String AssetPath::ToString() const
+{
+    if (HasSubAsset())
+    {
+        return String::Format("{}#{}", file_path, sub_asset_name);
+    }
+    return file_path.ToString();
+}
+}  // namespace se::asset
