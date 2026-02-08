f18b81ac7e92c53c47fadcfbd14db213e425280d
gudtldn <gudtldn1234@gmail.com>
2026-02-08 16:31:17 +0900

    refactor: 셰이더 수정

--- EngineCore/Shaders/Default.frag.hlsl
+++ EngineCore/Shaders/Default.frag.hlsl
@@ -6,14 +6,6 @@
 
 float4 main(PixelInput input) : SV_Target0
 {
-    int px = int(input.position.x);
-    int py = int(input.position.y);
-
-    if (px % 2 == 0 || py % 2 == 0)
-    {
-        discard;
-    }
-
     // 보간된 색상을 그대로 반환
     return input.color;
 }
