f2a5366324a3af61f5c49423730038804562565c
gudtldn <gudtldn1234@gmail.com>
2026-02-10 00:45:28 +0900

    feat: 커스텀 RTTI 기반 유틸리티 함수 추가

---
+++ EngineCore/Include/SimpleEngine/Core/Reflection/Cast.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <concepts>
+
+#include "SimpleEngine/Core/Reflection/TypeId.h"
+#include "SimpleEngine/Core/Reflection/TypeRegistry.h"
+
+
+namespace se
+{
+/**
+ * 리플렉션 기반 캐스팅이 가능한 타입인지 검사하는 Concept
+ * SE_CLASS 매크로를 통해 GetTypeId()가 구현된 타입만 사용 가능합니다.
+ */
+template <typename T>
+concept Castable = requires(const T& obj)
+{
+    { obj.GetTypeId() } -> std::same_as<TypeId>;
+};
+
+namespace detail
+{
+/**
+ * TypeId의 상속 체인을 순회하며, derived_id가 base_id와 같거나 파생 관계인지 확인합니다.
+ * @param derived_id 검사 대상 (파생 클래스) TypeId
+ * @param base_id 기준이 되는 (부모 클래스) TypeId
+ * @return derived_id가 base_id이거나, base_id의 파생 클래스이면 true
+ */
+[[nodiscard]] inline bool IsTypeDerivedFrom(const TypeId& derived_id, const TypeId& base_id)
+{
+    TypeId current = derived_id;
+    while (current.IsValid())
+    {
+        if (current == base_id)
+        {
+            return true;
+        }
+
+        const Optional info_opt = TypeRegistry::Get().Find(current);
+        if (!info_opt.HasValue())
+        {
+            break;
+        }
+
+        current = info_opt->base_or_inner_id;
+    }
+    return false;
+}
+} // namespace detail
+
+/**
+ * 객체가 지정한 타입 T이거나 T의 파생 클래스인지 검사합니다.
+ * @tparam T 검사할 대상 타입
+ * @param obj 검사할 객체의 포인터 (nullptr일 경우 false)
+ * @return obj의 런타임 타입이 T이거나 T로부터 파생되었으면 true
+ */
+template <typename T, Castable From>
+[[nodiscard]] bool IsA(const From* obj)
+{
+    if (!obj)
+    {
+        return false;
+    }
+    return detail::IsTypeDerivedFrom(obj->GetTypeId(), TypeId::Get<T>());
+}
+
+template <typename T, Castable From>
+[[nodiscard]] bool IsA(const From& obj)
+{
+    return detail::IsTypeDerivedFrom(obj.GetTypeId(), TypeId::Get<T>());
+}
+
+/**
+ * 두 타입의 상속 관계를 확인합니다.
+ * @tparam Derived 자식 타입
+ * @tparam Base 부모 타입
+ * @return Derived가 Base이거나 Base로부터 파생되었으면 true
+ */
+template <typename Derived, typename Base>
+[[nodiscard]] bool IsChildOf()
+{
+    return detail::IsTypeDerivedFrom(TypeId::Get<Derived>(), TypeId::Get<Base>());
+}
+
+/**
+ * 주어진 TypeId가 Base 타입의 자식(또는 동일한 타입)인지 검사합니다.
+ * @tparam Base 부모 타입
+ * @param derived_id 검사할 TypeId
+ * @return derived_id가 Base이거나 Base로부터 파생되었으면 true
+ */
+template <typename Base>
+[[nodiscard]] bool IsChildOf(TypeId derived_id)
+{
+    return detail::IsTypeDerivedFrom(derived_id, TypeId::Get<Base>());
+}
+
+/**
+ * 안전한 다운캐스팅을 수행합니다.
+ * 상속 체인을 검사한 뒤, 유효하지 않으면 nullptr을 반환합니다.
+ *
+ * @tparam To 캐스팅할 대상 타입
+ * @param obj 캐스팅할 객체의 포인터
+ * @return 캐스팅에 성공하면 To* 포인터, 실패하면 nullptr
+ */
+template <typename To, Castable From>
+[[nodiscard]] To* Cast(From* obj)
+{
+    // From이 To를 상속 받았는지? (up casting)
+    if constexpr (std::derived_from<From, To>)
+    {
+        return static_cast<To*>(obj);
+    }
+    else
+    {
+        // obj가 원래 From이었는지? (down casting)
+        if (IsA<To>(obj))
+        {
+            return static_cast<To*>(obj);
+        }
+    }
+    return nullptr;
+}
+
+template <typename To, Castable From>
+[[nodiscard]] const To* Cast(const From* obj)
+{
+    // From이 To를 상속 받았는지? (up casting)
+    if constexpr (std::derived_from<From, To>)
+    {
+        return static_cast<const To*>(obj);
+    }
+    else
+    {
+        // obj가 원래 From이었는지? (down casting)
+        if (IsA<To>(obj))
+        {
+            return static_cast<const To*>(obj);
+        }
+    }
+    return nullptr;
+}
+
+/**
+ * 다운캐스팅을 수행합니다.
+ * 캐스팅에 실패하거나 nullptr이 전달되면 Assert가 발생합니다.
+ * 릴리스 빌드에서는 검사 없이 static_cast를 수행합니다.
+ *
+ * @tparam To 캐스팅할 대상 타입
+ * @param obj 캐스팅할 객체의 포인터 (nullptr 불가)
+ * @return 캐스팅된 To* 포인터
+ */
+template <typename To, Castable From>
+[[nodiscard]] To* CastChecked(From* obj)
+{
+    SE_ASSERT(obj != nullptr, "CastChecked failed: Source pointer is null!");
+    SE_ASSERT(
+        IsA<To>(obj),
+        "CastChecked failed: Cannot cast '{}' to '{}'!",
+        obj->GetTypeId().GetName(), TypeId::Get<To>().GetName()
+    );
+    return static_cast<To*>(obj);
+}
+
+template <typename To, Castable From>
+[[nodiscard]] const To* CastChecked(const From* obj)
+{
+    SE_ASSERT(obj != nullptr, "CastChecked failed: Source pointer is null!");
+    SE_ASSERT(
+        IsA<To>(obj),
+        "CastChecked failed: Cannot cast '{}' to '{}'!",
+        obj->GetTypeId().GetName(), TypeId::Get<To>().GetName()
+    );
+    return static_cast<const To*>(obj);
+}
+
+/**
+ * 런타임 타입이 정확히 To와 일치하는 경우에만 캐스팅합니다.
+ * 상속 체인을 순회하지 않으므로, 파생 클래스는 매칭되지 않습니다.
+ *
+ * @tparam To 캐스팅할 대상 타입 (정확히 이 타입이어야 함)
+ * @param obj 캐스팅할 객체의 포인터
+ * @return 런타임 타입이 To와 동일하면 To* 포인터, 아니면 nullptr
+ */
+template <typename To, Castable From>
+[[nodiscard]] To* ExactCast(From* obj)
+{
+    if (obj && obj->GetTypeId() == TypeId::Get<To>())
+    {
+        return static_cast<To*>(obj);
+    }
+    return nullptr;
+}
+
+template <typename To, Castable From>
+[[nodiscard]] const To* ExactCast(const From* obj)
+{
+    if (obj && obj->GetTypeId() == TypeId::Get<To>())
+    {
+        return static_cast<const To*>(obj);
+    }
+    return nullptr;
+}
+} // namespace se
