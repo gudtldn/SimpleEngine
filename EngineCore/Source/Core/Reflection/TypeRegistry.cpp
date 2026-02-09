8659df8a67818ed4bba6c45e9416af52ae3acf0e
gudtldn <gudtldn1234@gmail.com>
2026-02-09 17:59:21 +0900

    refactor: TypeRegistry가 Builder를 반환하도록 리펙로딩. 추가로 RegisterPrimitive 추가

--- EngineCore/Source/Core/Reflection/TypeRegistry.cpp
+++ EngineCore/Source/Core/Reflection/TypeRegistry.cpp
@@ -9,12 +9,6 @@ TypeRegistry& TypeRegistry::Get()
     return instance;
 }
 
-void TypeRegistry::RegisterType(TypeInfo&& type_info)
-{
-    name_map[type_info.name] = type_info.type_id;
-    type_map[type_info.type_id] = std::move(type_info);
-}
-
 Optional<const TypeInfo&> TypeRegistry::Find(const TypeId& type_id) const
 {
     return type_map.Find(type_id);
@@ -26,5 +20,11 @@ Optional<const TypeInfo&> TypeRegistry::Find(const StringName& type_name) const
     {
         return Find(type_id);
     });
+}
+
+const TypeInfo& TypeRegistry::FindChecked(const TypeId& type_id) const
+{
+    SE_ASSERT(type_map.Contains(type_id), "Type '{}' is not registered yet! Make sure SE_END_REFLECT is called.", type_id.GetName());
+    return type_map.FindChecked(type_id);
 }
 }  // namespace se
