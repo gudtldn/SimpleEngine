5ff69ef94a9eb93feb0cd3f92011e7a866a1ba18
gudtldn <gudtldn1234@gmail.com>
2026-02-12 15:29:02 +0900

    fix: Archive 이름 변경

--- EngineCore/Include/SimpleEngine/Traits/SerializationTraits.h
+++ EngineCore/Include/SimpleEngine/Traits/SerializationTraits.h
@@ -4,14 +4,14 @@
 namespace se
 {
 // Forward declarations
-class ArchiveV2;
+class Archive;
 }  // namespace se
 
 namespace se::traits
 {
 /** Serialize 할 수 있는 타입만 받습니다. */
 template <typename T>
-concept Serializable = requires(ArchiveV2& ar, T& value)
+concept Serializable = requires(Archive& ar, T& value)
 {
     // ADL Serialize 존재 여부 판별
     Serialize(ar, value);
