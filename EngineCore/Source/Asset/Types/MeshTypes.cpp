6ba209be446a8acb1f44039a91b3fe47244d970d
gudtldn <gudtldn1234@gmail.com>
2026-02-14 15:59:22 +0900

    refactor: SkeletalMesh 리플렉션 태그 추가

--- EngineCore/Source/Asset/Types/MeshTypes.cpp
+++ EngineCore/Source/Asset/Types/MeshTypes.cpp
@@ -11,7 +11,7 @@ SE_BEGIN_REFLECT(StaticMesh, meta::Reflect)
     SE_REFLECT_PROPERTY(bounds, meta::Property, meta::ReadOnly)
 SE_END_REFLECT(StaticMesh)
 
-SE_BEGIN_REFLECT(SkeletalMesh)
+SE_BEGIN_REFLECT(SkeletalMesh, meta::Reflect)
     SE_REFLECT_PROPERTY(vertices, meta::Property, meta::ReadOnly)
     SE_REFLECT_PROPERTY(skin_vertices, meta::Property, meta::ReadOnly)
     SE_REFLECT_PROPERTY(indices, meta::Property, meta::ReadOnly)
