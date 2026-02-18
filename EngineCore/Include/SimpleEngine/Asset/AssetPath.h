21ea2c7e933f13cc673ebeac9c8b730a4629ef20
gudtldn <gudtldn1234@gmail.com>
2026-02-18 19:18:10 +0900

    feat: AssetPath 직렬화 지원 추가 및 클래스 주석 보완
    - AssetPath 클래스에 Serialize 함수 추가로 직렬화 및 역직렬화 지원
    - file_path와 sub_asset_name 필드를 직렬화 가능한 형태로 처리 구현
    - AssetPath 클래스에 정의된 주석을 보완하여 사용 예시와 세부 설명 추가

--- EngineCore/Include/SimpleEngine/Asset/AssetPath.h
+++ EngineCore/Include/SimpleEngine/Asset/AssetPath.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "SimpleEngine/Core/Container/String.h"
+#include "SimpleEngine/Core/Serialization/Archive.h"
 #include "SimpleEngine/Core/Types/Path.h"
 #include "SimpleEngine/Utility/HashUtils.h"
 
@@ -8,7 +9,10 @@
 namespace se::asset
 {
 /**
- * @todo docs
+ * Asset의 소스 파일 경로와 Sub-Asset 이름을 결합한 식별 경로
+ *
+ * "models/character.fbx#Mesh_Body" 형태로 표현되며,
+ * file_path("models/character.fbx")와 sub_asset_name("Mesh_Body")으로 구성됩니다.
  */
 class SE_CORE_API AssetPath
 {
@@ -28,6 +32,23 @@ public:
     [[nodiscard]] auto operator<=>(const AssetPath&) const = default;
 
 private:
+    friend void Serialize(Archive& ar, AssetPath& path)
+    {
+        String file_str;
+        if (ar.IsSaving())
+        {
+            file_str = path.file_path.ToString();
+        }
+
+        ar("file_path") << file_str;
+        ar("sub_asset_name") << path.sub_asset_name;
+
+        if (ar.IsLoading())
+        {
+            path.file_path = Path{ file_str };
+        }
+    }
+
     Path file_path;
     String sub_asset_name;
 };
