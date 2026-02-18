c2b04131d687df42bc836ed7a7abbe6752d4cc08
gudtldn <gudtldn1234@gmail.com>
2026-02-18 17:06:15 +0900

    feat: 리플렉션 트레이트 및 새로운 Concept 추가
    - 비침투형 리플렉션 타입 마킹을 위한 `ReflectionTraits` 구조체 도입
    - IntrusiveReflectable/NonIntrusiveReflectable/Reflectable 개념 분리 및 정의
    - 엔진 리플렉션 시스템의 유연성과 확장성을 위해 구조 최적화 및 코드 가독성 향상

---
+++ EngineCore/Include/SimpleEngine/Core/Reflection/Traits.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <concepts>
+
+
+namespace se
+{
+// forward declaration
+struct TypeId;
+struct TypeInfo;
+
+/**
+ * 비침투형(Non-intrusive) 리플렉션 타입을 컴파일 타임에 마킹하기 위한 traits 구조체
+ * SE_DECLARE_REFLECTION 매크로를 통해 특수화됩니다
+ */
+template <typename T>
+struct ReflectionTraits
+{
+    static constexpr bool Enabled = false;
+};
+
+/**
+ * SE_CLASS 매크로를 통해 침투형(Intrusive) 리플렉션이 구현된 타입인지 확인하는 Concept.
+ * StaticTypeInfo(), GetTypeInfo(), GetTypeId() 멤버를 요구합니다.
+ */
+template <typename T>
+concept IntrusiveReflectable = requires(const T& obj)
+{
+    { obj.StaticTypeInfo() } -> std::same_as<const TypeInfo&>;
+    { obj.GetTypeInfo() } -> std::same_as<const TypeInfo&>;
+    { obj.GetTypeId() } -> std::same_as<TypeId>;
+};
+
+/**
+ * SE_DECLARE_REFLECTION 매크로를 통해 비침투형(Non-intrusive) 리플렉션이 마킹된 타입인지 확인하는 Concept.
+ */
+template <typename T>
+concept NonIntrusiveReflectable = ReflectionTraits<T>::Enabled;
+
+/**
+ * 엔진 리플렉션 시스템에 등록된 타입인지 확인합니다.
+ * 침투형(SE_CLASS) 또는 비침투형(SE_DECLARE_REFLECTION) 어느 쪽이든 해당되면 true
+ */
+template <typename T>
+concept Reflectable = IntrusiveReflectable<T> || NonIntrusiveReflectable<T>;
+} // namespace se
