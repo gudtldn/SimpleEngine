61a42690caf7bb2855708a6eaab86a311acad920
gudtldn <gudtldn1234@gmail.com>
2026-02-27 20:39:18 +0900

    feat: EventSubsystem 추가 및 SDL 이벤트 처리 로직 구현
    - EventSubsystem 클래스 및 헤더 추가
    - SDL 이벤트 서브시스템 초기화 및 종료 처리 로직 구현
    - PollEvents 메서드에서 SDL 이벤트 폴링 및 브로드캐스트 로직 작성
    - on_sdl_event, on_quit_requested, on_window_close_requested 델리게이트 추가 및 이벤트 핸들링

---
+++ EngineCore/Include/SimpleEngine/Core/HAL/EventSubsystem.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "SimpleEngine/Core/Functional/MultiDelegate.h"
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+#include "SimpleEngine/Core/Reflection/Annotations.h"
+#include "SimpleEngine/Core/Subsystem/SubsystemBase.h"
+
+#include "SDL3/SDL.h"
+
+
+namespace se
+{
+/**
+ * SDL 이벤트 폴링 및 Raw 이벤트 브로드캐스팅을 담당하는 Subsystem
+ */
+class SE_CORE_API SE_ANNOTATION(=meta::Internal) EventSubsystem : public SubsystemBase
+{
+    SE_CLASS(EventSubsystem, SubsystemBase)
+
+public:
+    //~ Begin SubsystemBase
+    [[nodiscard]] virtual bool Initialize() override;
+    virtual void Release() override;
+    //~ End SubsystemBase
+
+public:
+    /** 매 프레임 호출하여 SDL 이벤트를 폴링하고 Delegate를 Broadcast합니다. */
+    void PollEvents();
+
+public:
+    /**
+     * SDL 이벤트가 발생할 때마다 Broadcast됩니다.
+     * ImGui 등 모든 raw SDL 이벤트를 받아야 하는 시스템용입니다.
+     */
+    MultiDelegate<void(SDL_Event&)> on_sdl_event;
+
+    /**
+     * SDL_EVENT_QUIT 이벤트 발생 시 Broadcast됩니다.
+     */
+    MultiDelegate<void()> on_quit_requested;
+};
+} // namespace se
