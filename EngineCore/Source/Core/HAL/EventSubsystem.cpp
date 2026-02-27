1cad4951db03dfe63adb35f95cae31020fc4c28d
gudtldn <gudtldn1234@gmail.com>
2026-02-27 20:39:18 +0900

    feat: EventSubsystem 추가 및 SDL 이벤트 처리 로직 구현
    - EventSubsystem 클래스 및 헤더 추가
    - SDL 이벤트 서브시스템 초기화 및 종료 처리 로직 구현
    - PollEvents 메서드에서 SDL 이벤트 폴링 및 브로드캐스트 로직 작성
    - on_sdl_event, on_quit_requested, on_window_close_requested 델리게이트 추가 및 이벤트 핸들링

---
+++ EngineCore/Source/Core/HAL/EventSubsystem.cpp
@@ -0,0 +1,56 @@
+﻿#include "SimpleEngine/Core/HAL/EventSubsystem.h"
+
+#include "SimpleEngine/Core/Logging/Logging.h"
+#include "SimpleEngine/Core/Reflection/Reflect.h"
+#include "SimpleEngine/Core/Subsystem/SubsystemRegistration.h"
+
+
+namespace se
+{
+SE_REGISTER_SUBSYSTEM(EventSubsystem);
+
+SE_BEGIN_REFLECT(EventSubsystem, meta::Internal)
+SE_END_REFLECT(EventSubsystem)
+
+
+bool EventSubsystem::Initialize()
+{
+    ConsoleLog(ELogLevel::Info, "Initializing Event Subsystem...");
+
+    if (!SDL_InitSubSystem(SDL_INIT_EVENTS))
+    {
+        ConsoleLog(ELogLevel::Error, "SDL_InitSubSystem failed: {}", SDL_GetError());
+        return false;
+    }
+
+    ConsoleLog(ELogLevel::Info, "Event Subsystem initialized");
+    return true;
+}
+
+void EventSubsystem::Release()
+{
+    ConsoleLog(ELogLevel::Info, "Releasing Event Subsystem...");
+
+    SDL_QuitSubSystem(SDL_INIT_EVENTS);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void EventSubsystem::PollEvents() // NOLINT(*-make-member-function-const)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        on_sdl_event.Broadcast(event);
+
+        switch (event.type)
+        {
+        case SDL_EVENT_QUIT:
+            on_quit_requested.Broadcast();
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+} // namespace se
