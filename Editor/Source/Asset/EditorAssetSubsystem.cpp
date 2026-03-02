972c5c9fcd3756be086b852caac038c3776568ee
gudtldn <gudtldn1234@gmail.com>
2026-03-02 20:06:54 +0900

    feat: ImportSettings 저장 방식 개선 및 In-place 객체 생성 지원
    - ImportProfile에 Emplace 메서드 추가로 ImportSettings In-place 저장 기능 구현
    - 기존 Set 메서드 개선 및 코드 리팩터링
    - Translator별 기본 ImportProfile 초기화 코드에서 Emplace 메서드 활용
    - 필요 없는 헤더 의존성 제거 및 정리

--- Editor/Source/Asset/EditorAssetSubsystem.cpp
+++ Editor/Source/Asset/EditorAssetSubsystem.cpp
@@ -54,7 +54,7 @@ bool EditorAssetSubsystem::Initialize()
     // Translator별 기본 ImportProfile 프리셋 등록
     preset_manager.RegisterPreset(TypeId::Get<AssimpTranslator>(), [](ImportProfile& profile)
     {
-        profile.Set(MeshImportSettings{});
+        profile.Emplace<MeshImportSettings>();
     });
 
     asset_subsystem = &GetSubsystemChecked<asset::AssetSubsystem>();
