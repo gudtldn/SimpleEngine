59ba0c3d15852f77ccb3ca7a783a649494c8755d
gudtldn <gudtldn1234@gmail.com>
2026-03-08 00:22:44 +0900

    feat: AssetMetadata에 의존성 관리 기능 추가
    - EAssetDependencyType 열거형으로 의존성 유형 정의
    - AssetDependencyEntry 구조체로 개별 의존성 항목 표현 및 직렬화 추가
    - AssetMetadata에 dependencies 필드 추가로 의존 에셋 목록 관리 지원
    - SE_DECLARE_REFLECTION 및 SE_REFLECT_PROPERTY로 의존성 반영 메타정보 구현

--- EngineCore/Source/Asset/AssetMetadata.cpp
+++ EngineCore/Source/Asset/AssetMetadata.cpp
@@ -4,6 +4,12 @@
 
 namespace se::asset
 {
+SE_BEGIN_REFLECT(AssetDependencyEntry, meta::SerializeOnly)
+    SE_REFLECT_PROPERTY(source_vpath, meta::Property)
+    SE_REFLECT_PROPERTY(asset_guid, meta::Property)
+    SE_REFLECT_PROPERTY(type, meta::Property)
+SE_END_REFLECT(AssetDependencyEntry)
+
 SE_BEGIN_REFLECT(SubAssetMeta, meta::SerializeOnly)
     SE_REFLECT_PROPERTY(name, meta::Property)
     SE_REFLECT_PROPERTY(guid, meta::Property)
@@ -17,5 +23,6 @@ SE_BEGIN_REFLECT(AssetMetadata, meta::SerializeOnly)
     SE_REFLECT_PROPERTY(source_size, meta::Property)
     SE_REFLECT_PROPERTY(cache_version, meta::Property)
     SE_REFLECT_PROPERTY(sub_assets, meta::Property)
+    SE_REFLECT_PROPERTY(dependencies, meta::Property)
 SE_END_REFLECT(AssetMetadata)
 }  // namespace se::asset
