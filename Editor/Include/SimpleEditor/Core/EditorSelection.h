35998d9106a807637f37ec3a3a76d6beb735334d
gudtldn <gudtldn1234@gmail.com>
2026-03-18 12:57:26 +0900

    feat: 에셋 브라우저 디렉토리 포커싱 로직 및 API 추가
    - EditorSelection에 SetActiveContentDir, GetActiveContentDir 함수 추가
    - active_content_dir 멤버 변수 추가로 포커싱된 디렉토리 상태 저장
    - 관련 주석 추가 및 Path 타입 의존성 헤더 포함

--- Editor/Include/SimpleEditor/Core/EditorSelection.h
+++ Editor/Include/SimpleEditor/Core/EditorSelection.h
@@ -3,6 +3,7 @@
 #include "SimpleEditor/EditorCommon.h"
 
 #include "SimpleEngine/Core/Container/HashSet.h"
+#include "SimpleEngine/Core/Types/Path.h"
 #include "SimpleEngine/ECS/Entity.h"
 
 
@@ -21,9 +22,17 @@ public:
 
     /** 마지막으로 선택된 Entity */
     [[nodiscard]] Optional<Entity> GetPrimarySelectedEntity() const;
+    
+    /** 에셋 브라우저에서 디렉토리를 포커싱 합니다. */
+    void SetActiveContentDir(const Path& path);
+
+    /** 에셋 브라우저에서 포커싱된 디렉토리를 가져옵니다. */
+    [[nodiscard]] const Path& GetActiveContentDir() const;
 
 private:
     HashSet<Entity> selected_entities;
+
+    Path active_content_dir;
     // TODO: 추후 Asset 선택 추가: HashSet<Guid> selected_assets;
 };
 } // namespace se::editor
