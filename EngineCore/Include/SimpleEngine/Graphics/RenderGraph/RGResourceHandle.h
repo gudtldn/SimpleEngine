eae5ec1857fe37a9768069a7a8ca0ae01fc441da
gudtldn <gudtldn1234@gmail.com>
2026-03-22 19:06:08 +0900

    wip, refactor: RenderGraph 리소스 핸들 구조 개선 및 해시 로직 추가
    - RGResourceHandle 구조체를 RGResourceHandleImpl 템플릿 구조로 리팩토링
    - 텍스처와 버퍼 리소스 핸들 별도로 구분 (RGTextureHandle, RGBufferHandle 추가)
    - Handle의 index와 version 정보를 합쳐 해시 계산 지원 (HashUtils 활용)
    - 리소스 상태 구분 및 안정성 향상 위한 논리적 버전 관리 추가

--- EngineCore/Include/SimpleEngine/Graphics/RenderGraph/RGResourceHandle.h
+++ EngineCore/Include/SimpleEngine/Graphics/RenderGraph/RGResourceHandle.h
@@ -1,30 +1,52 @@
-﻿#pragma once
-#include <compare>
+#pragma once
+
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+#include "SimpleEngine/Utility/HashUtils.h"
+
+#include <functional>
 #include <limits>
 
 
 namespace se::graphics
 {
+namespace detail
+{
 /**
- * Render Graph 내의 리소스를 가리키는 핸들
+ * RenderGraph의 리소스를 나타내는 Handle 구조체
  */
-struct RGResourceHandle
+template <typename Tag>
+struct RGResourceHandleImpl
 {
-    usize index = std::numeric_limits<usize>::max();
+    /** resource_nodes 배열 내의 인덱스 */
+    uint32 index = std::numeric_limits<uint32>::max();
 
-    [[nodiscard]] constexpr bool operator==(const RGResourceHandle&) const = default;
-    [[nodiscard]] constexpr auto operator<=>(const RGResourceHandle&) const = default;
+    /**
+     * 리소스의 논리적 버전을 식별하는 값
+     * @note 동일한 물리 리소스가 서로 다른 패스(Pass)에서 쓰기 작업 등으로 인해 상태가 변경될 때 이를 구분하기 위해 사용
+     */
+    uint32 version = 0;
 
-    [[nodiscard]] constexpr bool IsValid() const { return index != std::numeric_limits<usize>::max(); }
+    [[nodiscard]] constexpr bool IsValid() const { return index != std::numeric_limits<uint32>::max(); }
     [[nodiscard]] explicit constexpr operator bool() const { return IsValid(); }
-};
-}  // namespace se::graphics
 
-template<>
-struct std::hash<se::graphics::RGResourceHandle>
+    [[nodiscard]] constexpr bool operator==(const RGResourceHandleImpl&) const = default;
+};
+} // namespace detail
+
+/** Render Graph 텍스처 리소스 핸들 */
+using RGTextureHandle = detail::RGResourceHandleImpl<struct RGTextureTag>;
+
+/** Render Graph 버퍼 리소스 핸들 */
+using RGBufferHandle = detail::RGResourceHandleImpl<struct RGBufferTag>;
+} // namespace se::graphics
+
+template <typename Tag>
+struct std::hash<se::graphics::detail::RGResourceHandleImpl<Tag>> // NOLINT(*-dcl58-cpp)
 {
-    [[nodiscard]] size_t operator()(const se::graphics::RGResourceHandle& handle) const noexcept
+    size_t operator()(const se::graphics::detail::RGResourceHandleImpl<Tag>& handle) const noexcept
     {
-        return std::hash<usize>{}(handle.index);
+        usize hash = 0;
+        se::HashUtils::Combine(hash, handle.index, handle.version);
+        return hash;
     }
 };
