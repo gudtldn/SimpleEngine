21f9b91e54854bf5c3ae549386c1343f661973ec
gudtldn <gudtldn1234@gmail.com>
2026-03-28 17:29:11 +0900

    feat: RenderPassBase에 GPU 큐 타입 및 기본 반환 로직 추가
    - ERGPassQueue 열거형 추가로 지원 큐 타입 정의 (Graphics, Compute, Transfer)
    - GetQueueType 메서드 추가로 각 패스의 기본 GPU 큐 타입 반환 지원
    - 멀티 큐 실행 및 관리 로직 확장을 위한 기반 마련

--- EngineCore/Include/SimpleEngine/Graphics/RenderPass/RenderPassBase.h
+++ EngineCore/Include/SimpleEngine/Graphics/RenderPass/RenderPassBase.h
@@ -1,4 +1,6 @@
 #pragma once
+
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
 #include "SimpleEngine/Core/Reflection/Reflect.h"
 
 
@@ -6,6 +8,17 @@ namespace se::graphics
 {
 class RGExecutionContext;
 class RGSetupContext;
+
+/**
+ * 패스가 사용하는 GPU 큐 타입입니다.
+ * 현재는 어노테이션 용도이며, 향후 멀티 큐 실행 시 Executor가 이 값을 사용합니다.
+ */
+enum class ERGPassQueue : uint8
+{
+    Graphics,  // SDL_BeginGPURenderPass
+    Compute,   // SDL_BeginGPUComputePass
+    Transfer,  // SDL_BeginGPUCopyPass
+};
 
 /**
  * Render Graph의 각 렌더링 단계를 정의하기 위한 인터페이스
@@ -28,5 +41,11 @@ public:
     * 이 함수 내에서 실제 렌더링 커맨드를 커맨드 버퍼에 기록해야 합니다.
     */
     virtual void Execute(RGExecutionContext& context) = 0;
+
+    /**
+     * 이 패스가 사용하는 GPU 큐 타입을 반환합니다.
+     * 기본값은 Graphics이며, Compute/Transfer 패스는 이 함수를 override합니다.
+     */
+    [[nodiscard]] virtual ERGPassQueue GetQueueType() const { return ERGPassQueue::Graphics; }
 };
-}  // namespace se::graphics
+} // namespace se::graphics
