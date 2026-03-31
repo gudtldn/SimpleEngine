da20bf0313757a8486733c83ba3ed0ed851cac40
gudtldn <gudtldn1234@gmail.com>
2026-03-31 16:04:59 +0900

    feat: WorldContext 클래스 추가 및 ECS 기능 확장 준비
    - ECS World와 Phase별 Schedule을 단위로 관리하는 WorldContext 클래스 구현
    - Phase 스케줄 관리 및 시스템 추가/실행 기능 템플릿 메서드 작성
    - World와 Schedule 객체 관리 및 Query 주입 기반 시스템 바인딩 코드 구성

---
+++ EngineCore/Source/ECS/WorldContext.cpp
@@ -0,0 +1,13 @@
+﻿#include "SimpleEngine/ECS/WorldContext.h"
+#include "SimpleEngine/ECS/World.h"
+
+
+namespace se
+{
+WorldContext::WorldContext()
+    : world(std::make_unique<World>())
+{
+}
+
+WorldContext::~WorldContext() = default;
+} // namespace se
