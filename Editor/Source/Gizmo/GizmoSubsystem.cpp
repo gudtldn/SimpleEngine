0b01e0f3a976c6db93e9a92a33432aaaf0ff924a
gudtldn <gudtldn1234@gmail.com>
2026-04-09 01:54:52 +0900

    feat: 선택된 엔티티에 기즈모 렌더링 로직 추가

--- Editor/Source/Gizmo/GizmoSubsystem.cpp
+++ Editor/Source/Gizmo/GizmoSubsystem.cpp
@@ -1,6 +1,12 @@
 #include "SimpleEditor/Gizmo/GizmoSubsystem.h"
 
+#include "SimpleEditor/Core/EditorSubsystem.h"
+#include "SimpleEditor/UI/EditorViewportSubsystem.h"
+
+#include "SimpleEngine/Core/Math/TransformUtility.h"
 #include "SimpleEngine/Core/Subsystem/SubsystemRegistration.h"
+#include "SimpleEngine/ECS/Components/GlobalTransformComponent.h"
+#include "SimpleEngine/ECS/EntitySubsystem.h"
 #include "SimpleEngine/Graphics/RenderSubsystem.h"
 #include "SimpleEngine/Utility/SubsystemUtils.h"
 
@@ -23,5 +29,46 @@ bool GizmoSubsystem::Initialize()
 void GizmoSubsystem::Release()
 {
     draw_list.reset();
+}
+
+void GizmoSubsystem::DrawGizmos()
+{
+    draw_list->Clear();
+
+    const auto [editor_subsystem, entity_subsystem, viewport_subsystem] =
+        se::GetSubsystems<const EditorSubsystem, const EntitySubsystem, const EditorViewportSubsystem>();
+
+    if (!editor_subsystem || !entity_subsystem || !viewport_subsystem)
+    {
+        return;
+    }
+
+    const auto selected_entity_opt = editor_subsystem->GetSelection().GetPrimarySelectedEntity();
+    if (!selected_entity_opt)
+    {
+        return;
+    }
+
+    const World& world = entity_subsystem->GetMainWorld().GetWorld();
+    const auto global_tf = world.TryGetComponent<GlobalTransformComponent>(*selected_entity_opt);
+    if (!global_tf)
+    {
+        return;
+    }
+
+    // FIXME: 포커스가 끊기면 기즈모가 안보이게 됨
+    const auto vp_info = viewport_subsystem->GetFocusedViewportInfo();
+    if (!vp_info)
+    {
+        return;
+    }
+
+    const Vector3 position = math::TransformUtility::DecomposeTranslation(global_tf->value);
+    const Quaternion rotation = (vp_info->coordinate_space == ECoordinateSpace::Local)
+        ? math::TransformUtility::DecomposeRotation(global_tf->value)
+        : Quaternion::Identity();
+
+    renderer.SetMode(vp_info->gizmo_mode);
+    renderer.Draw(*draw_list, position, rotation, vp_info->render_view);
 }
 } // namespace se::editor
