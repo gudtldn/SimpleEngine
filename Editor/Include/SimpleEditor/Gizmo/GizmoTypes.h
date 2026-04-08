3f5a10f20338299edc8bc692518c2ba44df1ac71
gudtldn <gudtldn1234@gmail.com>
2026-04-09 00:32:53 +0900

    feat: 기즈모 타입 정의를 위한 별도 헤더 파일 추가 및 의존성 분리
    - EGizmoMode 열거형을 GizmoTypes.h로 이동하여 모듈화
    - EGizmoAxis 열거형 추가: 기즈모 축 및 평면 식별자 정의
    - EditorViewportSubsystem에서 EGizmoMode 관련 코드 제거 및 GizmoTypes.h 의존성 추가

---
+++ Editor/Include/SimpleEditor/Gizmo/GizmoTypes.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+
+
+namespace se::editor
+{
+/** 기즈모 조작 모드 */
+enum class EGizmoMode : uint8
+{
+    Translate, // 이동
+    Rotate,    // 회전
+    Scale      // 스케일
+};
+
+/** 기즈모 축 또는 평면 식별자 (Picking/Highlight에서 사용) */
+enum class EGizmoAxis : uint8
+{
+    None, // 선택 없음
+    X,    // X축 (빨강)
+    Y,    // Y축 (초록)
+    Z,    // Z축 (파랑)
+    XY,   // XY 평면
+    XZ,   // XZ 평면
+    YZ,   // YZ 평면
+    All   // 전체 (균등 스케일 등)
+};
+} // namespace se::editor
