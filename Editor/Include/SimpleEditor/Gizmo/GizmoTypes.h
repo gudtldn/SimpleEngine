342a073992d0104494052af383a5ae2f350401b1
gudtldn <gudtldn1234@gmail.com>
2026-04-09 19:17:50 +0900

    refactor: EGizmoAxis에 시작 정수를 명확하게 지정

--- Editor/Include/SimpleEditor/Gizmo/GizmoTypes.h
+++ Editor/Include/SimpleEditor/Gizmo/GizmoTypes.h
@@ -16,13 +16,13 @@ enum class EGizmoMode : uint8
 /** 기즈모 축 또는 평면 식별자 (Picking/Highlight에서 사용) */
 enum class EGizmoAxis : uint8
 {
-    None, // 선택 없음
-    X,    // X축 (빨강)
-    Y,    // Y축 (초록)
-    Z,    // Z축 (파랑)
-    XY,   // XY 평면
-    XZ,   // XZ 평면
-    YZ,   // YZ 평면
-    All   // 전체 (균등 스케일 등)
+    None = 0, // 선택 없음
+    X,        // X축 (빨강)
+    Y,        // Y축 (초록)
+    Z,        // Z축 (파랑)
+    XY,       // XY 평면
+    XZ,       // XZ 평면
+    YZ,       // YZ 평면
+    All       // 전체 (균등 스케일 등)
 };
 } // namespace se::editor
