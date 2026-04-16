174f1c1092b6c764d4c9b3acd46c2875283acb72
gudtldn <gudtldn1234@gmail.com>
2026-04-16 13:19:19 +0900

    refactor: EntityPickPass 구조 단순화 및 PickId 인코딩 로직 분리
    - EntityColorPickDrawInfo 구조체 제거
    - DrawCommand를 직접 사용하도록 수정
    - EntityPickId::Encode 로직을 통해 타입 안전한 인코딩 처리
    - 불필요한 헤더 파일 포함 정리 및 코드 경량화

--- Editor/Include/SimpleEditor/Picking/EntityPickPass.h
+++ Editor/Include/SimpleEditor/Picking/EntityPickPass.h
@@ -1,9 +1,7 @@
-﻿#pragma once
+#pragma once
 
 #include "SimpleEditor/EditorCommon.h"
-#include "SimpleEditor/Picking/EntityPickResult.h"
 
-#include "SimpleEngine/Asset/AssetId.h"
 #include "SimpleEngine/Graphics/RenderGraph/RGResourceHandle.h"
 #include "SimpleEngine/Graphics/RenderPass/RenderPassBase.h"
 #include "SimpleEngine/Graphics/View/RenderView.h"
@@ -17,14 +15,6 @@ class GpuResourceManager;
 
 namespace se::editor
 {
-/** 개별 오브젝트의 프레임 내 렌더링 정보 (피킹용) */
-struct EntityColorPickDrawInfo
-{
-    Matrix4x4 model_matrix;
-    EntityPickResult pick_id;
-    asset::AssetId mesh_id;
-};
-
 /**
  * SceneDrawData의 오브젝트를 피킹하는 패스
  */
@@ -50,7 +40,6 @@ private:
     const graphics::GpuResourceManager& gpu_manager;
     graphics::RenderView render_view;
 
-    Array<EntityColorPickDrawInfo> draw_infos;
     graphics::RGTextureHandle pick_target_handle;
     graphics::RGTextureHandle pick_depth_handle;
     Vector2f cursor_pos;
