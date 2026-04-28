e9082b0662a18281b6feeaeb3e4a0b465800e926
gudtldn <me@gudtldn.dev>
2026-04-28 23:04:18 +0900

    refactor: 뷰 모드 및 좌표계 타입 정의를 별도 헤더로 분리
    - ECoordinateSpace(좌표계 모드)와 EViewMode(뷰 모드) 열거형을 ViewModeTypes.h로 분리
    - EditorViewportSubsystem.h에서 관련 정의 제거 및 분할된 파일 참조 추가
    - 코드 가독성과 모듈성을 위해 관련 타입 정의 구조화

---
+++ Editor/Include/SimpleEditor/UI/ViewModeTypes.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "SimpleEngine/Core/HAL/PlatformTypes.h"
+
+
+namespace se::editor
+{
+/** 기즈모 좌표계 모드 */
+enum class ECoordinateSpace : uint8
+{
+    World,
+    Local,
+};
+
+/** 뷰포트 뷰 모드 */
+enum class EViewMode : uint8
+{
+    // 원근 뷰
+    Perspective,
+
+    // 직교 뷰
+    Top,
+    Bottom,
+    Front,
+    Back,
+    Right,
+    Left,
+};
+} // namespace se::editor
