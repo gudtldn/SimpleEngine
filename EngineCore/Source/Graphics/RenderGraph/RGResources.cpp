e8db689cf9306a814f44d719a817dc83ec1d1749
gudtldn <me@gudtldn.dev>
2026-09-18 18:49:00 +0900

    feat: 런타임 타입 정보(RTTI) 및 캐스트 기능 추가
    - SE_RTTI, SE_RTTI_ROOT 매크로로 자체 RTTI 지원 및 클래스 계층 정보 생성
    - Cast, ExactCast, IsA 유틸리티 함수로 동적 캐스팅 및 타입 체크 구현
    - TypeRecordOf 함수로 타입 메타데이터 조회 지원 및 글로벌 캐싱
    - 다중 상속, 인터페이스, 다이아몬드 상속 등 다양한 구조 지원 추가
    - 자체 RTTI 기반 성능 측정을 위한 벤치마크(Benchmark) 및 단위 테스트 추가

--- EngineCore/Source/Graphics/RenderGraph/RGResources.cpp
+++ EngineCore/Source/Graphics/RenderGraph/RGResources.cpp
@@ -1,5 +1,6 @@
 #include "SimpleEngine/Graphics/RenderGraph/RGResources.h"
 #include "../../../Include/SimpleEngine/Core/Reflection/Legacy/Reflect.h"
+#include "SimpleEngine/Core/Reflection/ReflectMacros.h"
 
 
 namespace se
@@ -25,3 +26,31 @@ SE_END_REFLECT_V1(RGTransientBuffer)
 SE_BEGIN_REFLECT_V1(RGExternalBuffer, meta::Reflect, meta::Hidden, meta::Transient)
 SE_END_REFLECT_V1(RGExternalBuffer)
 } // namespace se
+
+
+SE_REFLECT_BEGIN(se::RGResourceBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGTextureBase)
+    SE_BASE(se::RGResourceBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGBufferBase)
+    SE_BASE(se::RGResourceBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGTransientTexture)
+    SE_BASE(se::RGTextureBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGExternalTexture)
+    SE_BASE(se::RGTextureBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGTransientBuffer)
+    SE_BASE(se::RGBufferBase)
+SE_REFLECT_END()
+
+SE_REFLECT_BEGIN(se::RGExternalBuffer)
+    SE_BASE(se::RGBufferBase)
+SE_REFLECT_END()
