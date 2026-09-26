b99b9a58fd75bb06991ff714f2f8ec22f4fa4c10
gudtldn <me@gudtldn.dev>
2026-09-26 11:18:00 +0900

    feat: SerializeOpsRegistry와 내장 트레이트 등록 추가
    TypeId만으로 SerializeTraits<T>를 찾을 수 있도록, 트레이트의 Write/Read를 타입 소거한
    SerializeOps를 TypeId별로 등록합니다. SE_REGISTER_SERIALIZE_TRAITS(type)로 등록하며,
    같은 타입을 두 번 등록하면 assert합니다.
    
    내장 트레이트(String, StringName, Guid, TypeId, Path, VPath, ContentHash)를 등록합니다.

---
+++ EngineCore/Include/SimpleEngine/Core/Serialization/SerializeOpsRegistry.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "SimpleEngine/Core/Container/HashMap.h"
+#include "SimpleEngine/Core/Container/Optional.h"
+#include "SimpleEngine/Core/Reflection/TypeId.h"
+#include "SimpleEngine/Core/Serialization/SerializeTraits.h"
+#include "SimpleEngine/Utility/Common.h"
+
+
+namespace se
+{
+/**
+ * 타입 하나에 대한 직렬화 연산
+ */
+struct SerializeOps
+{
+    void (*write)(ArchiveWriter& writer, const void* value) = nullptr;
+    void (*read)(ArchiveReader& reader, void* value) = nullptr;
+};
+
+namespace detail
+{
+/** SerializeTraits<T>를 타입 소거된 SerializeOps로 만듭니다. */
+template <typename T>
+    requires HasSerializeTraits<T>
+SerializeOps MakeSerializeOps()
+{
+    return {
+        .write = [](ArchiveWriter& writer, const void* value) static
+        {
+            SerializeTraits<T>::Write(writer, *static_cast<const T*>(value));
+        },
+        .read = [](ArchiveReader& reader, void* value) static
+        {
+            SerializeTraits<T>::Read(reader, *static_cast<T*>(value));
+        },
+    };
+}
+} // namespace detail
+
+/**
+ * TypeId로 SerializeOps를 찾는 전역 레지스트리
+ * @note 단일 스레드 사용을 전제로 설계되었습니다.
+ */
+class SE_CORE_API SerializeOpsRegistry
+{
+    SerializeOpsRegistry() = default;
+
+public:
+    /** 전역 싱글톤 인스턴스를 가져옵니다. */
+    [[nodiscard]] static SerializeOpsRegistry& Get();
+
+    /** 주어진 타입의 SerializeOps를 등록합니다. 같은 TypeId가 이미 있으면 SE_ASSERT_RELEASE로 멈춥니다(덮어쓰지 않음). */
+    void Install(TypeId id, const SerializeOps& ops);
+
+    /** TypeId로 SerializeOps를 찾습니다. (등록되지 않았다면 NullOpt) */
+    [[nodiscard]] Optional<const SerializeOps&> Find(TypeId id) const;
+
+private:
+    HashMap<TypeId, SerializeOps> ops_map;
+};
+} // namespace se
+
+/**
+ * type의 SerializeTraits를 SerializeOpsRegistry에 등록합니다.
+ * @warning .cpp 파일에서만 사용하세요
+ */
+#define SE_REGISTER_SERIALIZE_TRAITS(type) \
+    static_assert(::se::HasSerializeTraits<type>, \
+        "SE_REGISTER_SERIALIZE_TRAITS(" #type "): SerializeTraits<" #type "> must provide " \
+        "Write(ArchiveWriter&, const T&) and Read(ArchiveReader&, T&)."); \
+    namespace { [[maybe_unused]] const bool SE_CONCAT_NAME(_se_serialize_kick_, __LINE__) = \
+        (::se::SerializeOpsRegistry::Get().Install(::se::TypeId::Of<type>(), ::se::detail::MakeSerializeOps<type>()), true); }
---
+++ EngineTest/Source/UnitTests/SerializeOpsRegistryTest.cpp
@@ -0,0 +1,83 @@
+#include "gtest/gtest.h"
+
+#include "SimpleEngine/Core/Reflection/TypeId.h"
+#include "SimpleEngine/Core/Serialization/BuiltinTraits.h"
+#include "SimpleEngine/Core/Serialization/SerializeOpsRegistry.h"
+
+
+// 내장 트레이트가 SerializeOpsRegistry에 연결되는지, 미등록 타입은 NullOpt인지 검증
+namespace se_serialize_ops_registry_test
+{
+using namespace se;
+
+/** 등록하지 않는 타입입니다. Find가 NullOpt를 돌려줘야 합니다. */
+struct NotRegistered
+{
+    i32 value = 0;
+};
+} // namespace se_serialize_ops_registry_test
+
+
+TEST(SerializeOpsRegistryTest, BuiltinStringIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::String>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinStringNameIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::StringName>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinGuidIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::Guid>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinTypeIdIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::TypeId>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinPathIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::Path>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinVPathIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::VPath>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, BuiltinContentHashIsInstalled)
+{
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<se::ContentHash>());
+    ASSERT_TRUE(ops.HasValue());
+    EXPECT_NE(ops->write, nullptr);
+    EXPECT_NE(ops->read, nullptr);
+}
+
+TEST(SerializeOpsRegistryTest, UnregisteredTypeIsNullOpt)
+{
+    using namespace se_serialize_ops_registry_test;
+
+    const auto ops = se::SerializeOpsRegistry::Get().Find(se::TypeId::Of<NotRegistered>());
+    EXPECT_FALSE(ops.HasValue());
+}
